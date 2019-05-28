7ab5b3b354a8687c59529617b797100b1c04c03d
Alcides Fonseca <me@alcidesfonseca.com>
2019-05-28 19:34:03 +0100

    Added setup script

---
+++ setup.sh
@@ -0,0 +1,10 @@
+pip3 install yapf
+pip3 install -r requirements.pip
+
+
+curl -o pre-commit.sh https://raw.githubusercontent.com/google/yapf/master/plugins/pre-commit.sh
+chmod a+x pre-commit.sh
+mv pre-commit.sh .git/hooks/pre-commit
+
+
+
