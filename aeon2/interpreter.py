02d8b6bef5ffa156e92754e038bcc9625362e7cd
Paulo Alexandre Canelas dos Santos <paulo.canelas.santos@hotmail.com>
2019-07-09 12:10:34 +0100

    Abstraction done. Application está quase. Sorry pelos prints

--- aeon2/interpreter.py
+++ aeon2/interpreter.py
@@ -5,11 +5,19 @@ from .ast import *
 def run(a: Program):
 
     ctx = {}
-    eval(ctx, a)
 
+    # Inserir native - talvez fazer modulo para isto
+    ctx['+'] = lambda x: lambda y: x + y
+    ctx['-'] = lambda x: lambda y: x - y
+    ctx['*'] = lambda x: lambda y: x * y
+    ctx['/'] = lambda x: lambda y: x / y
+    ctx['%'] = lambda x: lambda y: x % y
+    ctx['emptyList'] = []
+
+    evaluate(ctx, a)
     print("Interpreter: ", ctx)
 
-def eval(ctx, node):
+def evaluate(ctx, node):
 
     print(type(node), " :::::::::: ", node)
     nodeType = type(node)
@@ -21,53 +29,61 @@ def eval(ctx, node):
         elif type(node.type) is RefinedType:
             return convert(node.type.type.name, node.value)
         else:
-            print("Opsie aqui. Literal com tipo invalido.")
+            print("Literal com tipo invalido.")
     # Var - return the ctx value
     elif nodeType is Var:
-        # TODO: Temp fix - perguntar se coloco funcao implementada em python aqui
-        if node.name == "native":
-            return "native"
-        return ctx[node.name]
+        return ctx.get(node.name) # CUIDADO: retorna None para native functions e non-existing functions
     # Program
     elif nodeType is Program:
         for d in node.declarations:
-            eval(ctx, d)
+            print(30*'=')
+            evaluate(ctx, d)
     # Definition
     elif nodeType is Definition:
-        ctx[node.name] = eval(ctx, node.body)
+        if type(node.body) is Abstraction:
+            ctx[node.name] = evaluate(ctx, node.body) # eval(evaluate(ctx, node.body)) # descomentar depois
+        else:
+            bodyEval = evaluate(ctx, node.body)
+            # If it was a native function, it returns None, so we def non-native functions
+            if bodyEval is not None:
+                ctx[node.name] = bodyEval
     # Hole - nao acontece
     elif nodeType is Hole:
         pass
     # If - Executa a operacao resultado do then
     elif nodeType is If:
-        cond = eval(node.cond, ctx)
-        return cond and eval(ctx, node.then) or eval(ctx, node.otherwise)
+        cond = evaluate(ctx, node.cond)
+        return cond and evaluate(ctx, node.then) or evaluate(ctx, node.otherwise)
     # Import the statements
     elif nodeType is Import:
         # TODO: o prof tinha dito para passar a frente, mas presumo que tenha de
         # carregar para contexto o que esta no ficheiro  importar
         pass
     elif nodeType is Application:
+        # target ~> application, argument ~> Literal or Var
 
         target = node.target
         argument = node.argument
 
-        print(type(target), target)
-        print(type(argument), argument)
+        print("target: ", type(target), target)
+        print("argument: ", type(argument), argument)
+        # TODO: On hold
+        #targetEval = evaluate(ctx, target)
+        #argumentEval = evaluate(ctx, argument) and not None or argument.name
+
+        return "affz" #targetEval(argumentEval)
 
         print("Application: ", node)
+    # Abstraction - retorna representacao em string, convertida
     elif nodeType is Abstraction:
-        arg_type = node.arg_type #basictype
-        arg_name = node.arg_name #string
-        arg_body = node.body #abstraction
+        # criar contexto proprio para abstracoes????
+        # node.arg_type ~> basictype, node.arg_name ~>, node.body ~> abstraction
+        # print(">>>>>", type(node.body), node.body)
+        bodyEval = evaluate(ctx, node.body)
+        if bodyEval is None:
+            bodyEval = node.body.name
+        return "lambda {} : {}".format(node.arg_name, bodyEval)
 
-        print(type(arg_type), arg_type)
-        print(type(arg_name), arg_name)
-        print(type(arg_body), arg_body)
-        print("#")
-        eval(ctx, arg_body)
-
-        print("Abstraction: ", node)
     elif nodeType is TAbstraction:
         print("TAbstraction: ", node)
     elif nodeType is TApplication:
@@ -78,11 +94,10 @@ def eval(ctx, node):
         print("TypeDeclaration: ", node)
 
     else:
-        print("ERROR: ", node)
+        print("ERROR: ", type(node), node)
         return None
 
-
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # Converts the aeon basic value to a python value
 def convert(name, value):
     if name == 'Object' or name == 'Void':
@@ -100,6 +115,4 @@ def convert(name, value):
     else:
         print("ERROR: must define the default type: ", node.type)
         return None
-
-
-# ----------------------------------------------------------------------
+# ------------------------------------------------------------------------------
