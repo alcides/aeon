191be02921789d586aeec9e12b67ca02a910b846
Paulo Alexandre Canelas dos Santos <paulo.canelas.santos@hotmail.com>
2019-07-23 17:20:54 +0100

    Desenvolvimento Conversor. Converte Blocos, falta arranjar Edge case, falta natives e tipos

--- conversor/convert.py
+++ conversor/convert.py
@@ -5,7 +5,7 @@ import aeon2.types
 
 def convert(ast):
 
-    ctx = {}
+    ctx = defaultTypes()
     converted = []
 
     for node in ast:
@@ -43,38 +43,80 @@ def convert_node(ctx, node):
         return result
     # Declaracao de uma variavel
     elif nodet == 'let':
-        print("TODO: LET")
+        # Never happens
+        print("Convert: Should never reach here")
         return None
     # Declaracao de funcoes
     # ------------------ Function declaration and its elements -----------------
     elif nodet == "decl":
-        # TODO:
-        nName = node.nodes[0] # str
-        arguments = []
-        for arg in node.nodes[1]:
-            arguments.append(convert_node(ctx, arg))
-        nType = convert_node(ctx, node.nodes[2])
-        nBody = convert_node(ctx, node.nodes[-1])
+        # Node attributes
+        name = node.nodes[0]
+        args = node.nodes[1]
+        rType = node.nodes[2]
+        kind = node.nodes[3] # unkn1 => ...
+        dependentType = node.nodes[4] # where [x > 0]
+        unkn3 = node.nodes[5] # TODO: unknown??
+        block = node.nodes[6]
 
-        ctx[nName] = nType
-        return Definition(nName, nType, nBody)
+        ctx[name] = convert_node(ctx, rType)
+
+        result = "{} : ".format(name)
+
+        # TODO: falta tratar os que tem kinds
+        # kind = .............
+
+        for argument in args:
+            result = "{}{}".format(result, convert_node(ctx, argument))
+
+        result = "{}{}".format(result, convert_node(ctx, rType))
+
+        # TODO: Falta o tipo dependente
+        # dependentType = ..........
+
+        result = "{} = {}".format(result, convert_node(ctx, block))
+
+        return result
     # Tipo de retorno da funcao
     elif nodet == "rtype":
         name = node.nodes[0]
         typee = node.nodes[1]
-        result = convert_type(ctx, name, typee)
+        result = "{}:{}".format(name, convert_type(ctx, typee))
         return (typee.preconditions and '{{{}}}' or '({})').format(result)
+    # Argumentos da funcao
     elif nodet == "argument":
-        # TODO:
-        print("\n>>>>>>>> ARGUMENT: ", node, "\n\n")
-        return None
+        name = node.nodes[0]
+        typee = node.nodes[1]
+        result = "{}:{}".format(name, convert_type(ctx, typee))
+        result = (typee.preconditions and '{{{}}}' or '({})').format(result)
+        ctx[name] = result
+        return result + " -> "
     elif nodet == "block":
 
-        result = ""
+        result = "{}"
 
-        for instruction in node.nodes:
-            convert_node(ctx, instruction)
-            result = "\\x{} -> ".format("ola")
+        for instruction in node.nodes[:-1]:
+            if instruction.nodet == "let":
+                name = instruction.nodes[0]
+                assign = convert_node(ctx, instruction.nodes[1])
+                typee = convert_type(ctx, instruction.nodes[2])
+                strInstr = "(\\{}:{} -> {{}} ({}))".format(name, typee, assign)
+            else:
+                # TODO: Deal with edge case of literal and atom values
+                converted = convert_node(ctx, instruction)
+                strInstr = "(\\_:{} -> {{}} ({}))".format(ctx[instruction.nodet], converted)
+            result = result.format(strInstr)
+
+        lastInstruction = node.nodes[-1]
+
+        if lastInstruction.nodet == "let":
+            name = lastInstruction.nodes[0]
+            assign = convert_node(ctx, lastInstruction.nodes[1])
+            typee = convert_type(ctx, lastInstruction.nodes[2])
+            strInstr = "(\\{}:{} -> {})".format(name, typee, assign)
+        else:
+            converted = convert_node(ctx, lastInstruction)
+            strInstr = "(\\_:{} -> {})".format(ctx[lastInstruction.nodet], converted)
+        result = result.format(strInstr)
 
         return result
     else:
@@ -83,14 +125,33 @@ def convert_node(ctx, node):
 # ------------------------------------------------------------------------------
 # ----------------------------------- HELPERS ----------------------------------
 
-def convert_type(ctx, name, typee):
+def convert_type(ctx, typee):
     result = ""
     # TODO: Check if the others attributes are needed too
     if typee.preconditions:
         result = "{}".format(convert_node(ctx, typee.preconditions[0]))
         for cond in typee.preconditions[1:]:
             result = "{} and {}".format(result, convert_node(ctx, cond))
-        result = "{}:{} where {}".format(name, typee.type, result)
+        result = "{} where {}".format(typee.type, result)
     else:
-        result = "{}:{}".format(name, typee.type)
+        result = "{}".format(typee.type)
     return result
+
+def defaultTypes():
+    ctx = {}
+    ctx['+'] = "Integer"
+    ctx['-'] = "Integer"
+    ctx['*'] = "Integer"
+    ctx['/'] = "Integer"
+    ctx['%'] = "Integer"
+    ctx['>'] = "Boolean"
+    ctx['<'] = "Boolean"
+    ctx['>='] = "Boolean"
+    ctx['<='] = "Boolean"
+    ctx['=='] = "Boolean"
+    ctx['!='] = "Boolean"
+    ctx['==='] = "Boolean"
+    ctx['!=='] = "Boolean"
+    ctx['&&'] = "Boolean"
+    ctx['||'] = "Boolean"
+    return ctx
