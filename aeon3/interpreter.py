302ef26644e4edf38422d1fe1a8d2e95b5ee2ff2
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2019-11-07 13:51:24 +0000

    Interpreter refactored. AeonCore Translator

--- aeon3/interpreter.py
+++ aeon3/interpreter.py
@@ -2,84 +2,90 @@ from .types import *
 from .ast import *
 from .libraries.stdlib import *
 
+from multipledispatch import dispatch
 
 def run(node):
     ctx = nativeFunctions()
     return evaluate(ctx, node)
 
+@dispatch(dict, Literal)
 def evaluate(ctx, node):
+    return node.value
 
-    nodeType = type(node)
+@dispatch(dict, Var)
+def evaluate(ctx, node):
+    # CUIDADO: retorna None para native functions e non-existing functions
+    return ctx.get(node.name)
 
-    # Literal
-    if nodeType is Literal:
-        return node.value
-    # Var - return the ctx value
-    elif nodeType is Var:
-        return ctx.get(
-            node.name
-        )  # CUIDADO: retorna None para native functions e non-existing functions
-    # Program
-    elif nodeType is Program:
-        for d in node.declarations:
-            evaluate(ctx, d)
-    # Definition
-    elif nodeType is Definition:
-        # print(node.body)
-        if type(node.body) is Var:
-            pass
-            ''' TODO: confirmar esta remocao
-            bodyEval = evaluate(ctx, node.body)
-            print(">>>>>>", bodyEval)
-            # If it was ... = native, it returns None, so we only define non-native functions
-            if bodyEval is not None:
-                ctx[node.name] = bodyEval
-            '''
-        else:
-            ctx[node.name] = evaluate(ctx, node.body)
-    # Hole - nao acontece
-    elif nodeType is Hole:
+@dispatch(dict, Program)
+def evaluate(ctx, node):
+    for declaration in node.declarations:
+        evaluate(ctx, declaration)
+
+@dispatch(dict, Definition)
+def evaluate(ctx, node):
+    if type(node.body) is Var:
         pass
-    # If - Executa a operacao resultado do then
-    elif nodeType is If:
-        cond = evaluate(ctx, node.cond)
-        return evaluate(ctx, node.then) if cond else evaluate(
-            ctx, node.otherwise)
-    # Import - never happens
-    elif nodeType is Import:
-        pass
-    elif nodeType is Application:
-        # target ~> abstraction or application or var, argument ~> Literal or Var
-        target = evaluate(ctx, node.target)
-        if node.argument is None:
-            argument = 0 # TODO:
-        else:
-            argument = evaluate(ctx, node.argument)
-        # print("Target: ", node.target, target)
-        # print("Argument: ", argument, node.argument)
-        return target(argument)
-    # Abstraction - retorna representacao em string, convertida
-    elif nodeType is Abstraction:
-        # criar contexto proprio para abstracoes, a experimentar
-        return lambda x: evaluate(ctxPut(ctx.copy(), node.arg_name, x), node.body)
-    # TAbstraction - avaliar o corpo
-    elif nodeType is TAbstraction:
-        return evaluate(ctx, node.body)
-    # TApplication -
-    elif nodeType is TApplication:
-        return evaluate(ctx, node.target)
-    # TypeAlias - do later
-    elif nodeType is TypeAlias:
-        pass
-    # TypeDeclaration - do later
-    elif nodeType is TypeDeclaration:
-        pass
+        ''' TODO: confirmar esta remocao
+        bodyEval = evaluate(ctx, node.body)
+        print(">>>>>>", bodyEval)
+        # If it was ... = native, it returns None, so we only define non-native functions
+        if bodyEval is not None:
+            ctx[node.name] = bodyEval
+        '''
     else:
-        print("ERROR: ", type(node), node)
-        return None
+        ctx[node.name] = evaluate(ctx, node.body)
 
+@dispatch(dict, Hole)
+def evaluate(ctx, node):
+    pass
 
-## HELPER
+@dispatch(dict, If)
+def evaluate(ctx, node):
+    cond = evaluate(ctx, node.cond)
+    return evaluate(ctx, node.then) if cond else evaluate(ctx, node.otherwise)
+
+@dispatch(dict, Import)
+def evaluate(ctx, node):
+    pass
+
+@dispatch(dict, Application)
+def evaluate(ctx, node):
+    target = evaluate(ctx, node.target)
+    if node.argument is None:
+        argument = 0 # TODO:
+    else:
+        argument = evaluate(ctx, node.argument)
+    # print("Target: ", node.target, target)
+    # print("Argument: ", argument, node.argument)
+    return target(argument)
+
+@dispatch(dict, Abstraction)
+def evaluate(ctx, node):
+    return lambda x: evaluate(ctxPut(ctx.copy(), node.arg_name, x), node.body)
+
+@dispatch(dict, TAbstraction)
+def evaluate(ctx, node):
+    return evaluate(ctx, node.body)
+
+@dispatch(dict, TApplication)
+def evaluate(ctx, node):
+    return evaluate(ctx, node.target)
+
+@dispatch(dict, TypeAlias)
+def evaluate(ctx, node):
+    pass
+
+@dispatch(dict, TypeDeclaration)
+def evaluate(ctx, node):
+    pass
+    
+@dispatch(object, object)
+def evaluate(ctx, node):
+    raise Exception('Unknown node during evaluation:', type(node), node)
+
+# -----------------------------------------------------------------------------
+# ---------------------------------------------------------------------- HELPER
 
 def ctxPut(ctx, varName, var):
     if not varName.name.startswith('_'):
