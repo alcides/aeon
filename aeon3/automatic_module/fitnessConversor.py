8019957e74f9efea0c29e4aa1bb9757a0affe9ff
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2019-11-07 13:50:50 +0000

    First step into code synthesis. Random population

--- aeon3/automatic_module/fitnessConversor.py
+++ aeon3/automatic_module/fitnessConversor.py
@@ -5,10 +5,6 @@ from aeon3.ast import *
 
 import sys
 
-class Genetic(object):
-    pass
-
-
 def retrieve_fitness_functions(program):
     result = {}
     for declaration in program.declarations:
@@ -24,17 +20,56 @@ def get_holes(node):
     elif type(node) is Var:
         return []
     elif type(node) is If:
-        return has_hole(node.cond) + has_hole(node.then) + has_hole(node.otherwise)
+        return get_holes(node.cond) + get_holes(node.then) + get_holes(node.otherwise)
     elif type(node) is Application:
-        return has_hole(node.target) + has_hole(node.argument)
+        return get_holes(node.target) + get_holes(node.argument)
     elif type(node) is Abstraction:
-        return has_hole(node.body)
+        return get_holes(node.body)
     elif type(node) is TAbstraction:
-        return has_hole(node.body)
+        return get_holes(node.body)
     elif type(node) is TApplication:
-        return has_hole(node.target)
+        return get_holes(node.target)
     else:
         return []
+
+def replaceHoles(node, holes):
+    if type(node) is If:
+        if type(node.cond) is Hole:
+            node.cond = holes.pop(-1)
+        else:
+            replaceHoles(node.cond, holes)
+        if type(node.then) is Hole:
+            node.then = holes.pop(-1)
+        else:
+            replaceHoles(node.then, holes)
+        if type(node.otherwise) is Hole:
+            node.otherwise = holes.pop(-1)
+    elif type(node) is Application:
+        if type(node.target) is Hole:
+            node.target = holes.pop(-1)
+        else:
+            replaceHoles(node.target, holes)
+        if type(node.argument) is Hole:
+            node.argument = holes.pop(-1)
+        else:
+            replaceHoles(node.argument)
+    elif type(node) is Abstraction:
+        if type(node.body) is Hole:
+            node.body = holes.pop(-1)
+        else:
+            replaceHoles(node.body)
+    elif type(node) is TAbstraction:
+        if type(node.body) is Hole:
+            node.body = holes.pop(-1)
+        else:
+            replaceHoles(node.body)
+    elif type(node) is TApplication:
+        if type(node.target) is Hole:
+            node.target = holes.pop(-1)
+        else:
+            replaceHoles(node.body)
+    else:
+        raise Exception("Trying to replace unknown node: ", type(node), node)
 
 def generate_fitnesses(declaration):
     if type(declaration.return_type) is not RefinedType:
---
+++ aeon3/automatic_module/random.py
@@ -0,0 +1,39 @@
+import aeon2.synthesis as synthesis
+
+from .genProg import *
+from .fitnessConversor import replaceHoles
+
+
+class Random(GenProg):
+
+    def __init__(self, ctx, function, holes):
+        super(Random, self).__init__()
+        self.ctx = ctx
+        self.function = function
+        self.holes = holes
+
+    def crossover(self):
+        pass
+
+    def mutate(self):
+        pass
+    
+    def evaluate_fitness(self):
+        pass
+    
+    def run(self):
+        population = generatePopulation()
+        return population
+
+    def generatePopulation(self):
+
+        population = []
+
+        for i in range(self.POPULATION_SIZE):
+            individual_holes = [synthesis.se(self.ctx, hole, self.DEPTH) for hole in self.holes]
+            individual = self.function.copy()
+            individual.replaceHoles(individual, individual_holes)
+            population.append(individual)
+        
+        return population
+
