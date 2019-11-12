1759d405320a7bc5285ded6c97179382213f8b37
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2019-11-12 18:23:58 +0000

    Metrics: Depth, N-Nodes, Diversity

---
+++ aeon/tests/test_diversity.py
@@ -0,0 +1,26 @@
+import unittest
+import random
+
+from ..frontend import parse_strict
+from ..metrics.distance import compare_trees
+
+class TestSynthesis(unittest.TestCase):
+
+    def parse(self, text):
+        return parse_strict(text).declarations[0]
+
+    def diff_formula(self, value1, value2):
+        return (1 - pow(0.99, abs(value1 - value2)))
+
+    def test_literals(self):
+        ast1 = self.parse('10')
+        ast2 = self.parse('30')
+        
+        expected = self.diff_formula(ast1.value, ast2.value)
+        result = compare_trees(ast1, ast2)
+
+        self.assertEqual(expected, result)
+
+
+if __name__ == '__main__':
+    unittest.main()
