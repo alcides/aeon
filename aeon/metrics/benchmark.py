c3ed5130542bffff54f17eec34e2a8ecda116960
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2019-11-14 14:20:42 +0000

    Metricas

---
+++ aeon/metrics/benchmark.py
@@ -0,0 +1,20 @@
+from aeon.synthesis import se
+from aeon.metrics import Measurer
+from aeon.types import TypingContext, BasicType
+
+MAX_TREE_DEPTH = 3
+POPULATION_SIZE = 100
+
+def generate_and_benchmark(typee, file_writer):
+    population = list()
+    for i in range(POPULATION_SIZE):
+        try:    
+            ctx = TypingContext()
+            ctx.setup()
+            result = se(ctx, typee, MAX_TREE_DEPTH)
+            print('Generated individual:', result)
+            population.append(result)
+        except Exception as e: 
+            print("Not able to synthesise", e)
+    measurer = Measurer(file_writer)
+    measurer.measure(typee, population)
