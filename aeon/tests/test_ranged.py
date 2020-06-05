a6ce57cd3fefac4797b8bd23b88ea1d9b029b9c4
Alcides Fonseca <me@alcidesfonseca.com>
2020-06-05 16:18:46 +0100

    Renamed Ineq to Ranged Test.
    Run using: bash tests.sh test_ranged.TestRanged

--- aeon/tests/test_ranged.py
+++ aeon/tests/test_ranged.py
@@ -14,13 +14,8 @@ from aeon.translator import translate
 from aeon.typechecker.typing import synth_type
 from aeon.typechecker import check_type
 
-name = 'x'
 
-x = Symbol(name)
-RangedContext.Variable = name
-
-
-class TestInequalities(unittest.TestCase):
+class TestRanged(unittest.TestCase):
     def setUp(self):
         # Set the contexts
         self.ctx = TypingContext()
@@ -28,75 +23,67 @@ class TestInequalities(unittest.TestCase):
 
     def support_synthesis_with_ineq(self, typee):
         T = ty2(typee)
-
-        self.ctx = self.ctx
-        self.ctx.restrictions = list()
-        self.ctx.restrictions.append(T.cond)
-
-        value = ranged(self.ctx, T.type, T.name, [T.cond])
-
+        value = try_ranged(self.ctx, T)
         self.assertIsNotNone(value)
-
-        expression = Literal(value, T)
-
-        self.assertIsNotNone(check_type(self.ctx, expression, T))
+        e = Literal(value, T)
+        self.assertIsNotNone(check_type(self.ctx, e, T))
 
     "Integer"
 
-    def test_ineqs_int_closed_range(self):
+    def test_ranged_int_closed_range(self):
         self.support_synthesis_with_ineq(
             '{x:Integer where (x >= 0) && (x <= 10)}')
 
-    def test_ineqs_int_closed_range_with_hole(self):
+    def test_ranged_int_closed_range_with_hole(self):
         self.support_synthesis_with_ineq(
             '{x:Integer where ((x >= 0) && (x <= 10)) && (x != 5)}')
 
-    def test_ineqs_int_even(self):
+    def test_ranged_int_even(self):
         self.support_synthesis_with_ineq('{x:Integer where (x % 2) == 0}')
 
-    def test_ineqs_int_odd(self):
+    def test_ranged_int_odd(self):
         self.support_synthesis_with_ineq('{x:Integer where !((x % 2) == 0)}')
 
-    def test_ineqs_int_with_implies(self):
+    def test_ranged_int_with_implies(self):
         self.support_synthesis_with_ineq(
             '{x:Integer where ((x % 2) == 0) --> (x > 0)}')
 
-    def test_ineqs_int_with_lambda(self):
+    def test_ranged_int_with_lambda(self):
         self.support_synthesis_with_ineq(
             '{x:Integer where (\y:Integer -> x > y)(10)}')
 
     "Double"
 
-    def test_ineqs_double_not_range(self):
+    def test_ranged_double_not_range(self):
         self.support_synthesis_with_ineq(
             '{x:Double where !((x < 0.0) || (x > 10.0))}')
 
-    def test_ineqs_double_implies_and(self):
+    def test_ranged_double_implies_and(self):
         self.support_synthesis_with_ineq(
             '{x:Double where (x > 10.0) --> ((x >= 10.0) && (x <= 20.0))}')
 
     "Boolean"
 
-    def test_ineqs_bool_true(self):
+    def test_ranged_bool_true(self):
         self.support_synthesis_with_ineq('{x:Boolean where x == true}')
 
-    def test_ineqs_bool_true_or(self):
+    def test_ranged_bool_true_or(self):
         self.support_synthesis_with_ineq('{x:Boolean where x || true}')
 
-    def test_ineqs_bool_not_x(self):
+    def test_ranged_bool_not_x(self):
         self.support_synthesis_with_ineq('{x:Boolean where (x || true) --> x}')
 
     "String"
 
-    def test_ineqs_string_empty(self):
+    def test_ranged_string_empty(self):
         self.support_synthesis_with_ineq(
             '{x:String where (_String_size(x)) == 0}')
 
-    def test_ineqs_string_non_empty_up_to_10(self):
+    def test_ranged_string_non_empty_up_to_10(self):
         self.support_synthesis_with_ineq(
             '{x:String where (_String_size(x)) > 0 && (_String_size(x)) < 10}')
 
-    def test_ineqs_string_even_length(self):
+    def test_ranged_string_even_length(self):
         self.support_synthesis_with_ineq(
             '{x:String where (_String_size(x)) % 2 == 0}')
 
