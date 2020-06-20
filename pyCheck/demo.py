06c3e2a69cc42ed33f4763f7a91c2b667b99ec5c
Paulo Alexandre Canelas dos Santos <psantos@lasige.di.fc.ul.pt>
2020-06-20 16:09:01 +0100

    pyCheck update

--- pyCheck/demo.py
+++ pyCheck/demo.py
@@ -14,18 +14,21 @@ def abs_sum(x, y) :
 
     
 
-@provide('{x:Integer where x >= 0 && x <= 10}', '{y:Integer where y < 0}', repeat=100)
+@provide('{x:Integer | x >= 0 && x <= 10}', '{y:Integer | y < 0}', expected='{z:Integer | z == x - y}', repeat=100)
 def test_restricted_x(x, y):
     result = abs_sum(x, y)
-    expected = x - y
+    return result
 
-    assert(result == expected)
+
+
+
+
 
 # Wanted to do this in unittest, but have problems with self argument on decorator
 '''
 class TestAbsSum(unittest.TestCase):
     # pytest-quickcheck
-    @check('{x:int where x >= 0 and x <= 10}', '{y:int where y < 0}', repeat=100)
+    @provide('{x:int where x >= 0 and x <= 10}', '{y:int where y < 0}', repeat=100)
     def test_restricted_x(self, x, y):
         self.assertEquals(abs_sum(x, y), x - y)
 '''
--- pyCheck/pyCheck.py
+++ pyCheck/pyCheck.py
@@ -1,6 +1,14 @@
+from functools import reduce
+
 from aeon.frontend_core import typee, TypingContext
+
+from aeon.ast import Literal, Application, refined_value
+from aeon.types import t_f, t_i, t_b, t_s, AbstractionType, RefinedType
+
 from aeon.synthesis.synthesis import se
 from aeon.typechecker.typing import check_type
+
+from aeon.typechecker.substitutions import substitution_expr_in_type
 
 ty = typee.parse
 
@@ -8,6 +16,45 @@ def setUp():
     ctx = TypingContext()
     ctx.setup()
     return ctx
+
+def get_name(T):
+    if isinstance(T, RefinedType):
+        return T.name
+    if isinstance(T, AbstractionType):
+        return T.arg_name
+    return '_'
+    
+def get_T(value):
+    if isinstance(value, float):
+        return t_f
+    elif isinstance(value, int):
+        return t_i
+    elif isinstance(value, bool):
+        return t_b
+    elif isinstance(value, str):
+        return t_s
+    return None
+
+def assert_restriction(ctx, inputs, output, typees, return_type):
+
+        result = 1
+
+        return_value = Literal(output, refined_value(output, get_T(output)))
+
+        for value, typee in zip(inputs, typees):
+            name = get_name(typee)
+            value = Literal(value, refined_value(value, get_T(value)))
+            return_type = substitution_expr_in_type(return_type, value, name)
+
+        try:
+            check_type(ctx, return_value, return_type)
+            print("SUCCESS: Refined test passed for input values: {}".format(inputs))
+        except Exception:
+            print("ERROR: Refined test failed for input values: {}".format(inputs))
+            result = 0
+
+        return result
+        
 
 def provide(*args, **kwargs):
     def wrapper(function):
@@ -17,17 +64,19 @@ def provide(*args, **kwargs):
         # SetUp the refined input generator
         context = setUp()
         typees = [ty(param) for param in args]
+
+        # Obtain the kwargs
+        return_type = ty(kwargs['expected'])
         repeat = 1 if 'repeat' not in kwargs else kwargs['repeat']
 
         for _ in range(repeat):
             values = [se(context, T, 0).value for T in typees]
-            try:
-                function(*values)
-                print("SUCCESS: Refined test passed for input values: {}".format(values))
-                passed += 1
-            except AssertionError as err:
-                print("ERROR: Refined test failed for input values: {}".format(values))
-        
+            
+            # Run the function with the values
+            result = function(*values)
+
+            passed += assert_restriction(context, values, result, typees, return_type)
+                
         print()
         print("-"*80)
         print("Report:")
