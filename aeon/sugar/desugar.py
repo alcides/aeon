9ff19278b8098a06db45ea761e7a7a097871e1de
Alcides Fonseca <me@alcidesfonseca.com>
2021-04-04 21:29:56 +0100

    Printing main hole

--- aeon/sugar/desugar.py
+++ aeon/sugar/desugar.py
@@ -6,7 +6,7 @@ from aeon.sugar.program import Program
 from aeon.utils.ctx_helpers import build_context
 from aeon.prelude.prelude import typing_vars, evaluation_vars
 from aeon.backend.evaluator import EvaluationContext
-from aeon.core.terms import Abstraction, Literal, Rec, Term, Hole
+from aeon.core.terms import Abstraction, Application, Literal, Rec, Term, Hole, Var
 from aeon.core.types import AbstractionType, t_int
 from aeon.typing.context import TypeBinder, TypingContext
 
@@ -18,7 +18,7 @@ def desugar(p: Program) -> Tuple[Term, TypingContext, EvaluationContext]:
         ctx = TypeBinder(ctx, tyname)
     print(p)
 
-    prog = Hole("main")
+    prog = Application(Var("print"), Hole("main"))
     for d in p.definitions:
         ty = d.type
         body = d.body
