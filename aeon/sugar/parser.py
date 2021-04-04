09060b05e53b1839a043a83e51eea42bb0195e4e
Alcides Fonseca <me@alcidesfonseca.com>
2021-04-04 21:20:14 +0100

    Updated TODO

--- aeon/sugar/parser.py
+++ aeon/sugar/parser.py
@@ -2,9 +2,11 @@ from aeon.utils.ast_helpers import mk_binop
 import pathlib
 from lark import Lark, Transformer
 
+from aeon.frontend.parser import TreeToCore
 from aeon.core.substitutions import liquefy
 from aeon.core.terms import (
-    Abstraction, Annotation,
+    Abstraction,
+    Annotation,
     Application,
     Hole,
     If,
@@ -35,19 +37,7 @@ from aeon.utils.ast_helpers import (
 from aeon.sugar.program import Program, TypeDecl, Definition
 
 
-class TreeToSugar(Transformer):
-    counter: int
-
-    def __init__(self, start_counter=0):
-        self.counter = start_counter
-
-    def fresh(self) -> str:
-        self.counter += 1
-        return "_anf_{}".format(self.counter)
-
-    def same(self, args):
-        return args[0]
-
+class TreeToSugar(TreeToCore):
     def list(self, args):
         return args
 
@@ -71,109 +61,6 @@ class TreeToSugar(Transformer):
 
     def arg(self, args):
         return (args[0], args[1])
-
-    # Types
-    def refined_t(self, args):
-        return RefinedType(str(args[0]), args[1], liquefy(args[2]))
-
-    def abstraction_t(self, args):
-        return AbstractionType(str(args[0]), args[1], args[2])
-
-    def simple_t(self, args):
-        return BaseType(str(args[0]))
-
-    # Expressions
-
-    def int_lit(self, args):
-        return Literal(int(args[0]), type=t_int)
-
-    def float_lit(self, args):
-        return Literal(float(args[0]), type=t_float)
-
-    def bool_lit(self, args):
-        value = str(args[0]) == "true"
-        return Literal(value, type=t_bool)
-
-    def string_lit(self, args):
-        v = str(args[0])[1:-1]
-        return Literal(str(v), type=t_string)
-
-    def var(self, args):
-        return Var(str(args[0]))
-
-    def hole(self, args):
-        return Hole(str(args[0]))
-
-    def nnot(self, args):
-        return Application(Var("!"), args[0])
-
-    def binop_eq(self, args):
-        return self.binop(args, "==")
-
-    def binop_neq(self, args):
-        return self.binop(args, "!=")
-
-    def binop_and(self, args):
-        return self.binop(args, "&&")
-
-    def binop_or(self, args):
-        return self.binop(args, "||")
-
-    def binop_lt(self, args):
-        return self.binop(args, "<")
-
-    def binop_le(self, args):
-        return self.binop(args, "<=")
-
-    def binop_gt(self, args):
-        return self.binop(args, ">")
-
-    def binop_ge(self, args):
-        return self.binop(args, ">=")
-
-    def binop_imp(self, args):
-        return self.binop(args, "-->")
-
-    def binop_plus(self, args):
-        return self.binop(args, "+")
-
-    def binop_minus(self, args):
-        return self.binop(args, "-")
-
-    def binop_mult(self, args):
-        return self.binop(args, "*")
-
-    def binop_div(self, args):
-        return self.binop(args, "/")
-
-    def binop_mod(self, args):
-        return self.binop(args, "%")
-
-    def binop(self, args, op):
-        return mk_binop(lambda: self.fresh(), op, args[0], args[1])
-
-    def annotation(self, args):
-        return Annotation(args[0], args[1])
-
-    def minus(self, args):
-        if isinstance(args[0], Literal) and args[0].type == t_int:
-            return Literal(-args[0].value, args[0].type)
-        return mk_binop(lambda: self.fresh(), "-", i0, args[0])
-
-    def if_e(self, args):
-        return ensure_anf_if(lambda: self.fresh(), If(args[0], args[1], args[2]))
-
-    def let_e(self, args):
-        return ensure_anf_let(Let(str(args[0]), args[1], args[2]))
-
-    def rec_e(self, args):
-        return ensure_anf_rec(Rec(str(args[0]), args[1], args[2], args[3]))
-
-    def application_e(self, args):
-        return ensure_anf_app(lambda: self.fresh(), Application(args[0], args[1]))
-
-    def abstraction_e(self, args):
-        return Abstraction(str(args[0]), args[1])
 
 
 def mk_parser(rule="start", start_counter=0):
--- notes.md
+++ notes.md
@@ -1,6 +1,6 @@
 TODO:
 
-* Add custom types
+* Auto_rename in Types (and lambdas) for capture-avoiding substitions (deep in the tree)
+* Introduce holes and synthesis
 * Introduce Imports
 * Introduce non-liquid refinements
-* Introduce holes and synthesis
