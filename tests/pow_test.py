98cacc51a9c2fa561a161076604e1ed0d4cffe2e
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-03-30 23:56:44 +0100

    added " : " as a separator of the return type

--- tests/pow_test.py
+++ tests/pow_test.py
@@ -23,6 +23,7 @@ def test_anf():
     source = r"""
         type Unit;
         def math : Unit = native_import "math";
-        def test (b:{x:Int | 1 <= x && x <= 100}) -> (e:{y:Int | 1 <= y && y <= 100}) ->  Int { native "lambda x: lambda y: math.pow(x , y)"} 
-        """
+        def pow : (b: {c:Int | ((c >= 1)  && (c <= 100))}) -> (e:{d:Int | ((d >= 1) && (d <= 100))}) ->  Int = native "lambda x: lambda y: math.pow(x , y)";
+
+"""
     check_compile(source, top, 1)
