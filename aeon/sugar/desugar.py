fc27f0710f32abef68a1f44d541c3645d2272427
Alcides Fonseca <me@alcidesfonseca.com>
2023-04-02 19:31:21 +0100

    Import now looks into multiple paths

--- aeon/sugar/desugar.py
+++ aeon/sugar/desugar.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import os.path
+from pathlib import Path
+
 from aeon.backend.evaluator import EvaluationContext
 from aeon.core.substitutions import substitute_vartype_in_term
 from aeon.core.terms import Abstraction
@@ -14,17 +17,15 @@ from aeon.core.types import BaseType
 from aeon.core.types import t_int
 from aeon.prelude.prelude import evaluation_vars
 from aeon.prelude.prelude import typing_vars
-from aeon.sugar.program import Definition
-from aeon.sugar.program import TypeDecl
-from aeon.sugar.program import Program
-from aeon.sugar.program import ImportAe
 from aeon.sugar.parser import mk_parser
+from aeon.sugar.program import Definition
+from aeon.sugar.program import ImportAe
+from aeon.sugar.program import Program
+from aeon.sugar.program import TypeDecl
 from aeon.typechecking.context import TypingContext
 from aeon.typechecking.context import VariableBinder
 from aeon.utils.ctx_helpers import build_context
 
-import os
-import os.path
 
 def desugar(p: Program) -> tuple[Term, TypingContext, EvaluationContext]:
     ctx = build_context(typing_vars)
@@ -35,21 +36,20 @@ def desugar(p: Program) -> tuple[Term, TypingContext, EvaluationContext]:
         prog = Application(Var("main"), Literal(1, type=t_int))
     else:
         prog = Application(Var("print"), Hole("main"))
-        
+
     defs: list[Definition] = p.definitions
-    type_decls:list[TypeDecl] = p.type_decls
+    type_decls: list[TypeDecl] = p.type_decls
     imports: list[ImportAe] = p.imports
-    
+
     imp: ImportAe
     for imp in imports:
         import_p: Program = handle_import(imp.path)
-        
+
         defs = import_p.definitions + defs
         type_decls = import_p.type_decls + type_decls
 
     d: Definition
     for d in defs[::-1]:
-        
         if d.body == Var("uninterpreted"):
             ctx = VariableBinder(
                 ctx,
@@ -64,17 +64,26 @@ def desugar(p: Program) -> tuple[Term, TypingContext, EvaluationContext]:
                 body = Abstraction(a, body)
             prog = Rec(d.name, ty, body, prog)
 
-    tyname:TypeDecl
+    tyname: TypeDecl
     for tyname in type_decls:
         prog = substitute_vartype_in_term(prog, BaseType(tyname.name), tyname.name)
 
     return (prog, ctx, ectx)
 
 
-def handle_import(path:str)-> Program:
-    
-    filename = "libraries/" + path + ".ae"
-    path = os.path.abspath(filename)
-    assert os.path.exists(path), f"The library '{path}' does not exist. {path}"
-    import_file_data = open(path).read()
-    return mk_parser("program").parse(import_file_data)
+def handle_import(path: str) -> Program:
+    """Imports a given path, following the precedence rules of current folder,
+    AEONPATH."""
+    possible_containers = (
+        [Path.cwd()]
+        + [Path.cwd() / "libraries"]
+        + [Path(str) for str in os.environ.get("AEONPATH", ";").split(";") if str]
+    )
+    for container in possible_containers:
+        file = container / f"{path}.ae"
+        if file.exists():
+            contents = open(file).read()
+            return mk_parser("program").parse(contents)
+    raise Exception(
+        f"Could not import {path} in any of the following paths: " + ";".join([str(p) for p in possible_containers]),
+    )
