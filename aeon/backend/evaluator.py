47d00526690f50dbe98f12aa37339a6492bbbc46
Alcides Fonseca <me@alcidesfonseca.com>
2023-04-29 15:27:03 +0100

    Removed debug

--- aeon/backend/evaluator.py
+++ aeon/backend/evaluator.py
@@ -45,11 +45,7 @@ def eval(t: Term, ctx: EvaluationContext = EvaluationContext()):
     elif isinstance(t, Application):
         f = eval(t.fun, ctx)
         arg = eval(t.arg, ctx)
-        try:
-            e = f(arg)
-        except Exception as e:
-            print(t, "failed", f, arg)
-            raise e
+        e = f(arg)
         if isinstance(t.fun, Var) and t.fun.name == "native_import":
             globals()[arg] = e
         return e
