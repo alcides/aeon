2bc0a1d8e752edb41541ce73c4b9ea545d8415d5
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-05-02 19:38:08 +0100

    grammar extrated from core

--- aeon/__main__.py
+++ aeon/__main__.py
@@ -66,9 +66,9 @@ def create_dataclass_from_definition(definition: Definition, grammar_nodes: list
     new_class = type(definition.name, (parent_class, ), new_class_dict)
 
     #print(new_class.__name__, "\n", new_class.__annotations__, "\n")
-    
+
     def str_method(self):
-        #wrong representation
+        # wrong representation
         field_values = [
             f'("{str(getattr(self, field_name))}")' for field_name, _ in definition.args]
         return f"{definition.name} {' '.join(field_values)}"
@@ -94,14 +94,58 @@ def build_grammar_sugar(defs: list[Definition], type_decls: list[TypeDecl]) -> l
     return grammar_nodes
 
 
+def create_class_from_rec_term(term: Rec, grammar_nodes: list(type)):
+    fields = {}
+    t = term.var_type
+    while isinstance(t, AbstractionType):
+        # TODO replace basetype Int, Bool etc with <class 'int'>, <class 'bool'> etc
+        # TODO handle refined type
+        grammar_nodes, typ = find_class_by_name(
+            grammar_nodes, t.var_type.name)
+        fields[t.var_name] = typ
+        t = t.type
 
+    # TODO handle type top and bootom
+    if isinstance(t, (Top, Bottom)):
+        return grammar_nodes
+
+    parent_class_name = t.name
+
+    grammar_nodes, parent_class = find_class_by_name(
+        grammar_nodes, parent_class_name)
+
+    new_class_dict = {
+        '__annotations__': dict(fields)
+    }
+    new_class = type(term.var_name, (parent_class, ), new_class_dict)
+
+    #print(new_class.__name__, "\n", new_class.__annotations__, "\n")
+    # TODO
+    def str_method(self):
+        return f" "
+
+    new_class.__str__ = str_method
+    grammar_nodes.append(new_class)
+
+    return grammar_nodes
+
+
+def build_grammar_core(term: Term, grammar_nodes: list[type]) -> list[type]:
+    rec = term
+    while isinstance(rec, Rec):
+        grammar_nodes = create_class_from_rec_term(rec, grammar_nodes)
+        rec = rec.body
+    return grammar_nodes
 
 # fitness function just to test grammar from sugar
+
+
 def fitness_function(individual):
     guess = str(individual)
     target = "Image_draw_rectangle(Image_draw_rectangle(Image_draw_rectangle(Image_draw_rectangle(Image_draw_rectangle(canvas)(rect1_coord)(key_color))(rect2_coord)(white_color))(rect3_coord)(key_color))(rect4_coord)(key_color))(rect5_coord)(key_color)"
     lev_distance = distance(guess, target)
     return 1 / (lev_distance + 1)
+
 
 def test_geneticengine(g):
     alg = SimpleGP(
@@ -116,6 +160,11 @@ def test_geneticengine(g):
     )
     best = alg.evolve()
     return best
+
+# TODO
+def get_holes_type(prog: Term) -> dict(str, tuple(Union[Type, None], TypingContext)):
+    holes = {}
+    return
 
 
 if __name__ == "__main__":
@@ -132,7 +181,7 @@ if __name__ == "__main__":
         p, ctx, ectx, defs, type_decls = desugar(prog)
     if "-d" in sys.argv or "--debug" in sys.argv:
         print(p)
-        
+
     errors = check_type_errors(ctx, p, top)
     if errors:
         print("-------------------------------")
@@ -144,15 +193,26 @@ if __name__ == "__main__":
         print("-------------------------------")
 
     else:
+
+        # TODO  extract the nodes from p instead of defs and type_decls
+        holes = {}
+        holes = get_holes_type(p)
+        grammar_n: list[type] = build_grammar_core(p, [])
+
+        for cls in grammar_n:
+            print(cls)
+        print(len(grammar_n))
+        print("##############")
+
         # grammar from sugar
         synth_funcs = ([d for d in prog.definitions
                         if d.name.startswith("synth_")])
-        
+
         grammar_nodes: list[type] = build_grammar_sugar(defs, type_decls)
         for cls in grammar_nodes:
             print(cls)
         print(len(grammar_nodes))
-        
+
         _, starting_node = find_class_by_name(
             grammar_nodes, synth_funcs[0].body.var_value.type.name)
 
