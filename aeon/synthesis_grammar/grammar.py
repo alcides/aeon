b942155b35dd27b203a019594ba21583064fe791
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-05-10 18:49:44 +0100

    deleted useless functions from synthesis grammar

--- aeon/synthesis_grammar/grammar.py
+++ aeon/synthesis_grammar/grammar.py
@@ -35,7 +35,7 @@ from aeon.typechecking.typeinfer import synth
 from aeon.verification.horn import fresh
 
 
-# Probably change this methoad for another file
+# Probably move this methoad to another file
 def refined_to_unrefinedtype(ty: RefinedType) -> Type:
     return ty.type
 
@@ -110,12 +110,11 @@ def get_fitness_term(term: Rec) -> Term:
         raise NotImplementedError("Fitness function not found")
 
 
-def extract_fitness(term: Term, ctx: TypingContext) -> Callable[[Any], int]:
+def extract_fitness(term: Term, ctx: TypingContext):
     assert isinstance(term, Rec)
     fitness_term = get_fitness_term(term)
 
-    if isinstance(fitness_term, Abstraction):
-        return lambda k: eval(fitness_term.body, ctx.with_var(fitness_term.var_name, k))
+    print("fitness_body:", fitness_term)
 
 
 # dict (hole_name , (hole_type, hole_typingContext))
@@ -163,53 +162,3 @@ def get_holes_type(
         holes[t.name] = (ty, ctx)
 
     return holes
-
-
-# delete
-def create_dataclass_from_definition(definition: Definition, grammar_nodes: list(type)):
-    fields = {arg_name: arg_type for arg_name, arg_type in definition.args}
-
-    t = definition.type
-    while isinstance(t, AbstractionType):
-        # TODO replace basetype Int, Bool etc with <class 'int'>, <class 'bool'> etc
-        # TODO handle refined type
-        _, typ = find_class_by_name(grammar_nodes, t.var_type.name)
-        fields[t.var_name] = typ
-        t = t.type
-
-    # TODO handle type top and bottom
-    if isinstance(t, (Top, Bottom)):
-        return grammar_nodes
-
-    parent_class_name = t.name
-
-    grammar_nodes, parent_class = find_class_by_name(grammar_nodes, parent_class_name)
-
-    new_class_dict = {"__annotations__": dict(fields)}
-    new_class = type(definition.name, (parent_class,), new_class_dict)
-
-    # print(new_class.__name__, "\n", new_class.__annotations__, "\n")
-
-    def str_method(self):
-        field_values = [f'("{str(getattr(self, field_name))}")' for field_name, _ in fields]
-        return f"{definition.name} {' '.join(field_values)}"
-
-    new_class.__str__ = str_method
-    grammar_nodes.append(new_class)
-
-    return grammar_nodes
-
-
-def build_grammar_sugar(defs: list[Definition], type_decls: list[TypeDecl]) -> list[type]:
-    grammar_nodes: list[type] = []
-
-    for ty in type_decls:
-        if ty.name not in [cls.__name__ for cls in grammar_nodes]:
-            type_dataclass = type(ty.name, (ABC,), {})
-            grammar_nodes.append(type_dataclass)
-    for d in defs:
-        # TODO if it is uninterpreted do not create a dataclass ?
-        # if (not isinstance(d.type, AbstractionType)):
-        grammar_nodes = create_dataclass_from_definition(d, grammar_nodes)
-
-    return grammar_nodes
