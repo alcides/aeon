d913a8d9f9f262b69aa9e9c1d42da798aceead06
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-05-12 22:53:36 +0100

    remove unnecessary methods and comments

--- aeon/synthesis_grammar/grammar.py
+++ aeon/synthesis_grammar/grammar.py
@@ -2,16 +2,12 @@ from __future__ import annotations
 
 from abc import ABC
 from dataclasses import dataclass
-from typing import Any
-from typing import Callable
 from typing import Optional
 
 from geneticengine.core.decorators import abstract
 from geneticengine.core.grammar import extract_grammar
 from lark.lexer import Token
 
-from aeon.backend.evaluator import eval
-from aeon.backend.evaluator import EvaluationContext
 from aeon.core.substitutions import substitution_in_type
 from aeon.core.terms import Abstraction
 from aeon.core.terms import Annotation
@@ -23,17 +19,12 @@ from aeon.core.terms import Rec
 from aeon.core.terms import Term
 from aeon.core.terms import Var
 from aeon.core.types import AbstractionType
-from aeon.core.types import BaseType
 from aeon.core.types import Bottom
 from aeon.core.types import RefinedType
 from aeon.core.types import Top
 from aeon.core.types import Type
-from aeon.core.types import TypeVar
-from aeon.sugar.program import Definition
-from aeon.sugar.program import TypeDecl
 from aeon.typechecking.context import TypingContext
 from aeon.typechecking.typeinfer import synth
-from aeon.verification.horn import fresh
 
 prelude_ops = ["%", "/", "*", "-", "+", ">=", ">", "<=", "<", "!=", "==", "print", "native_import", "native"]
 
@@ -59,48 +50,6 @@ def find_class_by_name(class_name: str, grammar_nodes: list(type)) -> tuple[list
 
     else:
         raise Exception("Not implemented: " + class_name)
-
-
-def create_class_from_rec_term(term: Rec, grammar_nodes: list(type)) -> list(type):
-    fields = {}
-    t = term.var_type
-    while isinstance(t, AbstractionType):
-        term_var_type = refined_to_unrefinedtype(t.var_type) if isinstance(t.var_type, RefinedType) else t.var_type
-
-        grammar_nodes, cls = find_class_by_name(term_var_type.name, grammar_nodes)
-
-        var_name = t.var_name.value if isinstance(t.var_name, Token) else t.var_name
-
-        fields[var_name] = cls
-        t = t.type
-
-    # TODO handle type top and bottom
-    if isinstance(t, (Top, Bottom)):
-        return grammar_nodes
-
-    parent_class_name = t.name
-    grammar_nodes, parent_class = find_class_by_name(parent_class_name, grammar_nodes)
-
-    new_class_dict = {"__annotations__": dict(fields)}
-    new_class = type(term.var_name, (parent_class,), new_class_dict)
-
-    # print(new_class.__name__, "\n", new_class.__annotations__, "\n")
-    def str_method(self):
-        field_values = [f'("{str(getattr(self, field_name))}")' for field_name, _ in fields]
-        return f"{term.name} {' '.join(field_values)}"
-
-    new_class.__str__ = str_method
-    grammar_nodes.append(new_class)
-
-    return grammar_nodes
-
-
-def build_grammar_core(term: Term, grammar_nodes: list[type] = []) -> list[type]:
-    rec = term
-    while isinstance(rec, Rec):
-        grammar_nodes = create_class_from_rec_term(rec, grammar_nodes)
-        rec = rec.body
-    return grammar_nodes
 
 
 def get_fitness_term(term: Rec) -> Term:
@@ -135,7 +84,6 @@ def get_holes_type(
         holes = get_holes_type(ctx, t.body, ty, holes)
 
     elif isinstance(t, Let):
-        # not sure If the use of synth is the best option to get the type
         _, t1 = synth(ctx, t.var_value)
         ctx = ctx.with_var(t.var_name, t1)
         holes = get_holes_type(ctx, t.var_value, ty, holes)
@@ -228,8 +176,6 @@ def synthesis(ctx: TypingContext, p: Term, ty: Type):
     hole_type, hole_ctx = holes[first_hole]
 
     print(hole_ctx, ":", type(hole_ctx))
-
-    # build grammar nodes from context of the hole
 
     grammar_n = gen_grammar_nodes(hole_ctx)
 
