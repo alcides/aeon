24d67de56e24b8f5fee89c537d67ab1489b76085
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-05-31 11:12:14 +0100

    hole_type_dict_test changed

--- tests/hole_type_dict_test.py
+++ tests/hole_type_dict_test.py
@@ -11,14 +11,14 @@ from aeon.prelude.prelude import evaluation_vars
 from aeon.prelude.prelude import typing_vars
 from aeon.sugar.desugar import desugar
 from aeon.sugar.parser import parse_program
-from aeon.synthesis_grammar.grammar import get_holes_type
+from aeon.synthesis_grammar.grammar import get_holes_info
 from aeon.typechecking.typeinfer import check_type
 from aeon.utils.ctx_helpers import build_context
 
 
 def check_hole_type(source, hole_name, expected_type):
     p, ctx, _ = desugar(parse_program(source))
-    holes = get_holes_type(ctx, p, top)
+    holes = get_holes_info(ctx, p, top)
 
     assert holes[hole_name][0] == expected_type
 
