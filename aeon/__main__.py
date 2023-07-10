93c4eee03f3e902679a6b088040286e986c29af4
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-07-10 22:45:05 +0100

    logger added

--- aeon/__main__.py
+++ aeon/__main__.py
@@ -6,6 +6,7 @@ from aeon.backend.evaluator import eval
 from aeon.backend.evaluator import EvaluationContext
 from aeon.core.types import top
 from aeon.frontend.parser import parse_term
+from aeon.logger.logger import setup_logger
 from aeon.prelude.prelude import evaluation_vars
 from aeon.prelude.prelude import typing_vars
 from aeon.sugar.desugar import desugar
@@ -17,13 +18,18 @@ from aeon.utils.ctx_helpers import build_context
 
 if __name__ == "__main__":
 
-    parser = argparse.ArgumentParser(description="A simple argument parser")
+    parser = argparse.ArgumentParser(description="A simple parser")
 
     parser.add_argument("filename", type=str, help="name of the aeon files to be synthesized")
     parser.add_argument("--core", action="store_true", help="synthesize a aeon core file")
-    parser.add_argument("-d", "--debug", action="store_true", help="debug")
+#   parser.add_argument("-d", "--debug", action="store_true", help="debug")
+#   parser.add_argument("-typecheck", action="store_true", help="typechecker")
+    parser.add_argument("-l", "--log", type=str, nargs='+', default="", help="set log level")
+    parser.add_argument("-f", "--logfile", action="store_true", help="export log file")
 
     args = parser.parse_args()
+
+    logger = setup_logger(args.log, args.filename) if args.logfile else setup_logger(args.log)
 
     with open(args.filename) as f:
         code = f.read()
@@ -36,18 +42,18 @@ if __name__ == "__main__":
         prog: Program = parse_program(code)
         p, ctx, ectx = desugar(prog)
 
-    if args.debug:
-        print(p)
+    logger.info(p)
 
     errors = check_type_errors(ctx, p, top)
     if errors:
-        print("-------------------------------")
-        print("+  Type Checking Error        +")
+    #if errors and args.typecheck:
+        logger.log("TYPECHECKER", "-------------------------------")
+        logger.log("TYPECHECKER", "+  Type Checking Error        +")
         for error in errors:
-            print("-------------------------------")
-            print(error)
+            logger.log("TYPECHECKER", "-------------------------------")
+            logger.log("TYPECHECKER", error)
 
-        print("-------------------------------")
+        logger.log("TYPECHECKER", "-------------------------------")
 
     else:
         Synthesizer(ctx, p, top, ectx)
---
+++ aeon/logger/logger.py
@@ -0,0 +1,25 @@
+import sys
+
+from loguru import logger
+import datetime
+
+
+def levels_filter(levels):
+    def filter(record):
+        return record["level"].name in levels
+    return filter
+
+
+def setup_logger(logs: str, logfile_name: str = None):
+    logger.level("TYPECHECKER", no=35, color="<magenta>", icon="🔍")
+    # Setup the logger
+    logger.remove()
+    #logger.add(sys.stderr, level="DEBUG")
+    if not logfile_name:
+        logger.add(sys.stderr, filter=levels_filter(logs))
+    else:
+        logfile = f"logs/{logfile_name}_{datetime.datetime.now()}.log"
+        logger.add(logfile, filter=levels_filter(logs))
+
+    return logger
+
