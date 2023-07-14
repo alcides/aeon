e1249bb1f1807235d0cd0e29c06de850080af705
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-07-14 16:46:02 +0100

    export log function signature updated

--- aeon/logger/logger.py
+++ aeon/logger/logger.py
@@ -20,7 +20,7 @@ def setup_logger():
     return logger
 
 
-def export_log(logs: str = "", export_file: bool = False, logfile_name: str = None):
+def export_log(logs: list, export_file: bool = False, logfile_name: str = None):
 
     if export_file:
         logfile = f"logs/{logfile_name}_{datetime.datetime.now()}.log"
