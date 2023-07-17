c0dcbb7d18432067d35b5212b92c240e669c1a67
Alcides Fonseca <me@alcidesfonseca.com>
2023-07-17 10:28:23 +0100

    Fixed the problem with native and whitespace

--- aeon/sugar/parser.py
+++ aeon/sugar/parser.py
@@ -10,22 +10,21 @@ from aeon.core.types import AbstractionType
 from aeon.core.types import TypeVar
 from aeon.frontend.parser import TreeToCore
 from aeon.sugar.program import Definition
+from aeon.sugar.program import ImportAe
 from aeon.sugar.program import Program
 from aeon.sugar.program import TypeDecl
-from aeon.sugar.program import ImportAe
 
 
 class TreeToSugar(TreeToCore):
-
     def list(self, args):
         return args
 
     def program(self, args):
         return Program(args[0], args[1], args[2])
-    
+
     def regular_imp(self, args):
         return ImportAe(args[0], [])
-    
+
     def function_imp(self, args):
         return ImportAe(args[1], args[0])
 
@@ -61,9 +60,7 @@ def mk_parser(rule="start", start_counter=0):
         # lexer='standard',
         start=rule,
         transformer=TreeToSugar(start_counter),
-        import_paths=[
-            pathlib.Path(__file__).parent.parent.absolute() / "frontend"
-        ],
+        import_paths=[pathlib.Path(__file__).parent.parent.absolute() / "frontend"],
     )
 
 
