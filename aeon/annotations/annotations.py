d7c4513ba8c71ea73f8c8f0111ffb9983dff877d
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-08-24 13:57:05 +0100

    annotations module added

---
+++ aeon/annotations/annotations.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+
+from aeon.core.terms import Application
+from aeon.core.terms import Let
+from aeon.core.terms import Term
+from aeon.core.terms import Var
+from aeon.utils.ast_helpers import ensure_anf_app
+
+
+def minimize(term: Term) -> Term:
+    if isinstance(term, Var):
+        # TODO: Calculate the difference bewtween the minimum value and the variable
+        # then return that binary expression in the anf format
+        raise Exception("Term not handled by @minimize annotation")
+
+    elif isinstance(term, (Application, Let)):
+
+        return term
+
+    raise Exception("Term not handled by @minimize annotation " + str(type(term)))
+
+
+def maximize(term: Term) -> Term:
+    if isinstance(term, Var):
+        # TODO: Calculate the difference bewtween the maximumvalue and the variable
+        # then return that binary expression in the anf format
+        raise Exception("Term not handled by @maximize annotation")
+
+    elif isinstance(term, (Application, Let)):
+        return term
+
+    raise Exception("Term not handled by @maximize annotation " + str(type(term)))
