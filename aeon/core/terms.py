ff6617f131bec06ddf42d930100661a8beb62a2a
Alcides Fonseca <me@alcidesfonseca.com>
2023-09-26 01:41:14 +0100

    Made the AST of dataclasses

--- aeon/core/terms.py
+++ aeon/core/terms.py
@@ -14,13 +14,10 @@ class Term(ABC):
         return str(self).__hash__()
 
 
+@dataclass
 class Literal(Term):
     value: object
     type: Type
-
-    def __init__(self, value, type: Type):
-        self.value = value
-        self.type = type
 
     def __repr__(self):
         if self.type == t_string:
@@ -33,11 +30,9 @@ class Literal(Term):
             Literal) and self.value == other.value and self.type == other.type
 
 
+@dataclass
 class Var(Term):
     name: str
-
-    def __init__(self, name: str):
-        self.name = name
 
     def __str__(self):
         return f"{self.name}"
@@ -49,13 +44,10 @@ class Var(Term):
         return isinstance(other, Var) and self.name == other.name
 
 
+@dataclass
 class Annotation(Term):
     expr: Term
     type: Type
-
-    def __init__(self, expr: Term, type: Type):
-        self.expr = expr
-        self.type = type
 
     def __str__(self):
         return f"({self.expr} : {self.type})"
@@ -67,11 +59,9 @@ class Annotation(Term):
         return isinstance(other, Annotation) and self.expr == other.expr
 
 
+@dataclass
 class Hole(Term):
     name: str
-
-    def __init__(self, name: str):
-        self.name = name
 
     def __str__(self):
         return f"?{self.name}"
@@ -83,13 +73,10 @@ class Hole(Term):
         return isinstance(other, Hole) and self.name == other.name
 
 
+@dataclass
 class Application(Term):
     fun: Term
     arg: Term
-
-    def __init__(self, fun: Term, arg: Term):
-        self.fun = fun
-        self.arg = arg
 
     def __repr__(self):
         return f"({self.fun} {self.arg})"
@@ -100,13 +87,10 @@ class Application(Term):
             Application) and self.fun == other.fun and self.arg == other.arg
 
 
+@dataclass
 class Abstraction(Term):
     var_name: str
     body: Term
-
-    def __init__(self, var_name: str, body: Term):
-        self.var_name = var_name
-        self.body = body
 
     def __repr__(self):
         return f"(\\{self.var_name} -> {self.body})"
@@ -117,15 +101,11 @@ class Abstraction(Term):
         ) and self.var_name == other.var_name and self.body == other.body
 
 
+@dataclass
 class Let(Term):
     var_name: str
     var_value: Term
     body: Term
-
-    def __init__(self, var_name: str, var_value: Term, body: Term):
-        self.var_name = var_name
-        self.var_value = var_value
-        self.body = body
 
     def __str__(self):
         return f"(let {self.var_name} = {self.var_value} in\n\t{self.body})"
@@ -136,18 +116,12 @@ class Let(Term):
                 and self.body == other.body)
 
 
+@dataclass
 class Rec(Term):
     var_name: str
     var_type: Type
     var_value: Term
     body: Term
-
-    def __init__(self, var_name: str, var_type: Type, var_value: Term,
-                 body: Term):
-        self.var_name = var_name
-        self.var_type = var_type
-        self.var_value = var_value
-        self.body = body
 
     def __repr__(self):
         return str(self)
@@ -167,15 +141,11 @@ class Rec(Term):
                 and self.body == other.body)
 
 
+@dataclass
 class If(Term):
     cond: Term
     then: Term
     otherwise: Term
-
-    def __init__(self, cond: Term, then: Term, otherwise: Term):
-        self.cond = cond
-        self.then = then
-        self.otherwise = otherwise
 
     def __str__(self):
         return f"(if {self.cond} then {self.then} else {self.otherwise})"
