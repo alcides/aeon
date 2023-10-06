e1b8940e89748b01853844de3007320bba4b03c4
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2023-10-06 11:19:53 +0100

    minimize_flag substituted with objectives_list in main

--- aeon/__main__.py
+++ aeon/__main__.py
@@ -4,6 +4,7 @@ import argparse
 
 from aeon.backend.evaluator import eval
 from aeon.backend.evaluator import EvaluationContext
+from aeon.core.terms import Term
 from aeon.core.types import top
 from aeon.frontend.parser import parse_term
 from aeon.logger.logger import export_log
@@ -12,8 +13,9 @@ from aeon.prelude.prelude import evaluation_vars
 from aeon.prelude.prelude import typing_vars
 from aeon.sugar.desugar import desugar
 from aeon.sugar.parser import parse_program
-from aeon.sugar.program import Program
+from aeon.sugar.program import Program, Macro
 from aeon.synthesis_grammar.synthesizer import Synthesizer
+from aeon.typechecking.context import TypingContext
 from aeon.typechecking.typeinfer import check_type_errors
 from aeon.utils.ctx_helpers import build_context
 
@@ -40,11 +42,13 @@ def read_file(filename: str) -> str:
         return file.read()
 
 
-def process_code(core: bool, code: str) -> tuple:
+def process_code(
+    core: bool, code: str
+) -> tuple[Term, TypingContext, EvaluationContext, dict[str, tuple[Term, list[Macro]]]]:
     if core:
         context = build_context(typing_vars)
         evaluation_ctx = EvaluationContext(evaluation_vars)
-        return parse_term(code), context, evaluation_ctx, None
+        return parse_term(code), context, evaluation_ctx, {}
     else:
         prog: Program = parse_program(code)
         return desugar(prog)  # TODO Eduardo: This should return a list of Holes/Objectives
@@ -57,27 +61,26 @@ if __name__ == "__main__":
     export_log(args.log, args.logfile, args.filename)
 
     aeon_code = read_file(args.filename)
-    p, ctx, ectx, minimize_flag = process_code(args.core, aeon_code)
+    p, ctx, ectx, objectives_list = process_code(args.core, aeon_code)
     logger.info(p)
 
     errors = check_type_errors(ctx, p, top)
     if errors:
         logger.error("TYPECHECKER", "-------------------------------")
-        logger.error("TYPECHECKER", "+  Type Checking Error        +")
+        logger.error(f"+  Type Checking Error{ errors}        +")
         for error in errors:
             logger.error("TYPECHECKER", "-------------------------------")
             logger.error("TYPECHECKER", error)
         logger.error("TYPECHECKER", "-------------------------------")
     else:
-        synthesizer = Synthesizer(ctx, p, top, ectx)
-
-        if len(synthesizer.holes) <= 1:
-            eval(p, ectx)
-        else:
+        if objectives_list:
+            synthesizer = Synthesizer(ctx, p, top, ectx)
             file_name = args.filename if args.csv_synth else None
             best_solution = synthesizer.synthesize(
                 file_name,
-                minimize_flag,
+                objectives_list,
             )
 
             print(f"Best solution: {best_solution.genotype}")
+        else:
+            eval(p, ectx)
