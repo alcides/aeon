5d68880ea9c1721ffa7aa1c0350e8d15acb86737
Alcides Fonseca <me@alcidesfonseca.com>
2024-03-02 15:48:40 +0000

    Removed unused imports

--- tests/sugar_test.py
+++ tests/sugar_test.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
 from aeon.core.types import BaseType
-from aeon.core.types import Top
 from aeon.sugar.desugar import desugar
 from aeon.sugar.parser import parse_program
 from aeon.typechecking.typeinfer import check_type
-from aeon.utils.ctx_helpers import build_context
 
 
 def test_sugar():
@@ -15,8 +13,7 @@ def test_sugar():
 
     def k : (i:Int) -> Int = N.f;
 
-    """,
-    )
+    """, )
 
     t, ctx, _ = desugar(p)
     x = check_type(ctx, t, BaseType("Int"))
