6b5f729c540d11adafbfea851cf7d81be7d37919
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2024-03-06 12:31:16 +0000

    syn_ignore decorator added

--- aeon/decorators/api.py
+++ aeon/decorators/api.py
@@ -1,7 +1,7 @@
 from typing import Any, Callable
+
 from aeon.core.terms import Term
 from aeon.sugar.program import Definition
 
 Metadata = dict[str, Any]
-DecoratorType = Callable[[list[Term], Definition, Metadata],
-                         tuple[Definition, list[Definition], Metadata]]
+DecoratorType = Callable[[list[Term], Definition, Metadata], tuple[Definition, list[Definition], Metadata]]
--- aeon/synthesis_grammar/decorators.py
+++ aeon/synthesis_grammar/decorators.py
@@ -1,12 +1,15 @@
 """Meta-programming code for optimization-related decorators."""
 
 import aeon.synthesis_grammar.grammar as grammar
-from aeon.core.terms import Term
+from aeon.core.terms import Term, Var
 from aeon.core.types import BaseType
+from aeon.decorators.api import Metadata
 from aeon.sugar.program import Definition
 from aeon.synthesis_grammar.utils import fitness_function_name_for
 
-from aeon.decorators.api import Metadata
+
+def raise_decorator_error(name: str) -> None:
+    raise Exception(f"Exception in decorator named {name}.")
 
 
 def minimize_int(
@@ -71,4 +74,30 @@ def ignore(args: list[Term], fun: Definition, metadata: Metadata) -> tuple[Defin
     assert len(args) == 0
     grammar.internal_functions.append(fun.name)
     # internal_function = Definition(name=internal_name_for(fun.name), args=fun.args, type=fun.type, body=fun.body)
+    return fun, [], metadata
+
+
+def syn_ignore(args: list[Term], fun: Definition, metadata: Metadata) -> tuple[Definition, list[Definition], Metadata]:
+    """This decorator expects a zero argument .
+
+    It does not modify the original definition. It makes sure that no
+    grammar node is generated from this function.
+    """
+    assert len(args) != 0
+
+    # TODO How can I verify if the function is in the context?
+    def get_var_name(arg):
+        if isinstance(arg, Var):
+            return arg.name
+        else:
+            raise_decorator_error("syn_ignore")
+
+    # rethink this
+    aux_dict = {"syn_ignore": [get_var_name(arg) for arg in args]}
+    assert isinstance(fun.name, str)
+    if fun.name in metadata.keys():
+        metadata[(str(fun.name))].update(aux_dict)
+    else:
+        metadata[(str(fun.name))] = aux_dict
+
     return fun, [], metadata
