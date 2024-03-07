db7b435410ec6ee8afbd39a5ca9022391e570309
eduardomadeira98 <joseeduardomadeira@hotmail.com>
2024-03-07 23:47:54 +0000

    liquid types syntax test added to liquid_test

--- tests/liquid_test.py
+++ tests/liquid_test.py
@@ -6,7 +6,17 @@ from aeon.core.liquid import LiquidVar
 from aeon.core.substitutions import liquefy
 from aeon.core.terms import Application
 from aeon.core.terms import Var
+from aeon.core.types import top
+from aeon.sugar.desugar import desugar
+from aeon.sugar.parser import parse_program
+from aeon.typechecking.typeinfer import check_type
 from aeon.utils.ast_helpers import i1
+
+
+def check_compile(source, ty, res):
+    p, ctx, ectx = desugar(parse_program(source))
+    assert check_type(ctx, p, ty)
+
 
 l1 = LiquidLiteralInt(1)
 lx = LiquidVar("x")
@@ -31,3 +41,16 @@ def test_simple_eq():
         [LiquidLiteralInt(2)],
     )
     assert LiquidApp("x", [LiquidLiteralInt(1)]) != LiquidApp("x", [LiquidVar("x2")])
+
+
+def test_liquid_types_syntax():
+    source = r"""
+        def test (n:Int | n > 0) (z:Int) : Int {
+            n + z
+        }
+
+        def main (x:Int) : Unit {
+            print(test 5 5)
+        }
+"""
+    check_compile(source, top, 1)
