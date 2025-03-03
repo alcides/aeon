5e58a710a154db25c0a696a1ba0e9061830d9f16
Diogo Sousa <sousanamain@gmail.com>
2025-03-03 23:41:13 +0000

    - Refactor code in order to pass flake8 analysis

--- aeon/lsp/diagnostic.py
+++ aeon/lsp/diagnostic.py
@@ -1,15 +1,18 @@
 import logging
-from . import buildout
 from typing import AsyncIterable
+
 from lsprotocol.types import (Diagnostic)
 from pygls.server import LanguageServer
 
+from . import buildout
+
 logger = logging.getLogger(__name__)
+
 
 async def getDiagnostics(
     ls: LanguageServer,
     uri: str,
 ) -> AsyncIterable[Diagnostic]:
-  ast = await buildout.parse(ls, uri)
-  for diag in ast.diagnostics:
-      yield diag
+    ast = await buildout.parse(ls, uri)
+    for diag in ast.diagnostics:
+        yield diag
--- aeon/lsp/server.py
+++ aeon/lsp/server.py
@@ -13,13 +13,13 @@ from lsprotocol.types import (
     DidChangeWatchedFilesParams,
     DidOpenTextDocumentParams,
 )
-
 from pygls.server import LanguageServer
 
 from . import buildout, diagnostic
 
 server = LanguageServer("aeon.lsp.server", "0.1.0")
 DEBOUNCE_DELAY = 0.3
+
 
 def start_language_server_mode(tcp_server):
     if not tcp_server:
@@ -30,15 +30,16 @@ def start_language_server_mode(tcp_server):
     print(f'Listening on {host}:{port}')
     server.start_tcp(host, int(port))
 
+
 async def parseAndSendDiagnostics(
     ls: LanguageServer,
     uri: str,
 ) -> None:
-  await asyncio.sleep(DEBOUNCE_DELAY)
-  diagnostics = []
-  async for diag in diagnostic.getDiagnostics(ls, uri):
-    diagnostics.append(diag)
-  ls.publish_diagnostics(uri, diagnostics)
+    await asyncio.sleep(DEBOUNCE_DELAY)
+    diagnostics = []
+    async for diag in diagnostic.getDiagnostics(ls, uri):
+        diagnostics.append(diag)
+    ls.publish_diagnostics(uri, diagnostics)
 
 
 @server.feature(TEXT_DOCUMENT_DID_OPEN)
@@ -46,7 +47,7 @@ async def did_open(
     ls: LanguageServer,
     params: DidOpenTextDocumentParams,
 ) -> None:
-  await parseAndSendDiagnostics(ls, params.text_document.uri)
+    await parseAndSendDiagnostics(ls, params.text_document.uri)
 
 
 @server.feature(TEXT_DOCUMENT_DID_CHANGE)
@@ -54,8 +55,8 @@ async def did_change(
     ls: LanguageServer,
     params: DidChangeTextDocumentParams,
 ) -> None:
-  buildout.clearCache(params.text_document.uri)
-  await parseAndSendDiagnostics(ls, params.text_document.uri)
+    buildout.clearCache(params.text_document.uri)
+    await parseAndSendDiagnostics(ls, params.text_document.uri)
 
 
 @server.feature(WORKSPACE_DID_CHANGE_WATCHED_FILES)
@@ -63,8 +64,8 @@ async def did_change_watched_file(
     _: LanguageServer,
     params: DidChangeWatchedFilesParams,
 ) -> None:
-  for change in params.changes:
-    buildout.clearCache(change.uri)
+    for change in params.changes:
+        buildout.clearCache(change.uri)
 
 
 @server.feature(TEXT_DOCUMENT_COMPLETION, CompletionOptions(trigger_characters=["= "]))
@@ -72,26 +73,26 @@ async def lsp_completion(
     ls: LanguageServer,
     params: CompletionParams,
 ) -> Optional[List[CompletionItem]]:
-  await asyncio.sleep(DEBOUNCE_DELAY)
-  return [] #TODO
-  # items: List[CompletionItem] = []
-  #
-  # ast = await buildout.parse(ls, params.text_document.uri, True)
-  # for line in ast.lines:
-  #   pos = params.position
-  #   (var_name, var_type, value) = line
-  #   ci = CompletionItem(
-  #       label=var_name,
-  #       text_edit=TextEdit(
-  #           range=Range(start=Position(line=pos.line, character=pos.character),
-  #                       end=Position(line=pos.line,
-  #                                    character=pos.character + len(var_name))),
-  #           new_text=var_name,
-  #       ),
-  #       kind=CompletionItemKind.Variable,
-  #       documentation=MarkupContent(
-  #           kind=MarkupKind.Markdown,
-  #           value=f"{var_name} : {var_type} = {value}",
-  #       ))
-  #   items.append(ci)
-  # return items
+    await asyncio.sleep(DEBOUNCE_DELAY)
+    return []  # TODO
+    # items: List[CompletionItem] = []
+    #
+    # ast = await buildout.parse(ls, params.text_document.uri, True)
+    # for line in ast.lines:
+    #   pos = params.position
+    #   (var_name, var_type, value) = line
+    #   ci = CompletionItem(
+    #       label=var_name,
+    #       text_edit=TextEdit(
+    #           range=Range(start=Position(line=pos.line, character=pos.character),
+    #                       end=Position(line=pos.line,
+    #                                    character=pos.character + len(var_name))),
+    #           new_text=var_name,
+    #       ),
+    #       kind=CompletionItemKind.Variable,
+    #       documentation=MarkupContent(
+    #           kind=MarkupKind.Markdown,
+    #           value=f"{var_name} : {var_type} = {value}",
+    #       ))
+    #   items.append(ci)
+    # return items
