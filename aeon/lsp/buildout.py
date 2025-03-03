e75b438c58125952e35d7ec9463e0da6f230ebcc
Diogo Sousa <sousanamain@gmail.com>
2025-03-03 23:39:30 +0000

    - Fix tab spacing - Remove repeated code from server.py

--- aeon/lsp/buildout.py
+++ aeon/lsp/buildout.py
@@ -19,16 +19,14 @@ import io
 import logging
 import re
 import urllib.parse
-
 from typing import Dict, List, TextIO
-from lsprotocol.types import Diagnostic, DiagnosticSeverity, TEXT_DOCUMENT_DID_OPEN, DidOpenTextDocumentParams, \
-    DidChangeTextDocumentParams, TEXT_DOCUMENT_DID_CHANGE
-
-from pygls.server import LanguageServer
-from lsprotocol.types import Position, Range
 
 import requests
 from lark.exceptions import UnexpectedToken
+from lsprotocol.types import Diagnostic, DiagnosticSeverity
+from lsprotocol.types import Position, Range
+from pygls.server import LanguageServer
+
 from aeon.core.types import top
 from aeon.frontend.anf_converter import ensure_anf
 from aeon.sugar.desugar import desugar
@@ -40,32 +38,34 @@ requests_session = requests.Session()
 
 server = LanguageServer(name="aeon.languageserver", version="1.0.0")
 
-### type definitions ###
+# type definitions #
 URI = str  # type alias
 
-class AST:
-  def __init__(self, core_ast_anf, typing_ctx, diagnostics):
-    self.core_ast_anf = core_ast_anf
-    self.typing_ctx = typing_ctx
-    self.diagnostics = diagnostics
 
-### cache ###
+class AST:
+    def __init__(self, core_ast_anf, typing_ctx, diagnostics):
+        self.core_ast_anf = core_ast_anf
+        self.typing_ctx = typing_ctx
+        self.diagnostics = diagnostics
+
+
+# cache #
 
 # a cache of un-resolved buildouts by uri
 _parse_cache: Dict[URI, AST] = {}
 
 
 def clearCache(uri: URI) -> None:
-  """Clear all caches for uri.
+    """Clear all caches for uri.
 
-  This is to be called when the document is modified.
-  """
-  logger.debug("Clearing cache for %s", uri)
-  _parse_cache.pop(uri,None)
-  logger.debug("DONE!")
+    This is to be called when the document is modified.
+    """
+    logger.debug("Clearing cache for %s", uri)
+    _parse_cache.pop(uri, None)
+    logger.debug("DONE!")
 
 
-### buildout copied & modified functions ###
+# buildout copied & modified functions #
 
 _isurl = re.compile('([a-zA-Z0-9+.-]+)://').match
 
@@ -75,40 +75,40 @@ async def parse(
     uri: URI,
     allow_errors: bool = True,
 ) -> AST:
-  """
-  Parse a sectioned setup file and return a non-resolved buildout.
+    """
+    Parse a sectioned setup file and return a non-resolved buildout.
 
-  This is a wrapper over _parse which uses language server's workspace to access documents.
-  Returned value changed to a BuildoutProfile instance.
+    This is a wrapper over _parse which uses language server's workspace to access documents.
+    Returned value changed to a BuildoutProfile instance.
 
-  """
-  if uri in _parse_cache:
-    return copy.deepcopy(_parse_cache[uri])
+    """
+    if uri in _parse_cache:
+        return copy.deepcopy(_parse_cache[uri])
 
-  parsed_uri = urllib.parse.urlparse(uri)
-  if parsed_uri.scheme in (
-      'http',
-      'https',
-  ):
-    try:
-      fp = io.StringIO(requests_session.get(uri).text)
-    except requests.exceptions.ConnectionError:
-      fp = io.StringIO('')
-  else:
-    document = ls.workspace.get_text_document(uri)
-    try:
-      fp = io.StringIO(document.source)
-    except IOError:
-      if not allow_errors:
-        raise
-      fp = io.StringIO('')
-  parsed = await _parse(
-      fp,
-      uri,
-      allow_errors,
-  )
-  _parse_cache[uri] = copy.deepcopy(parsed)
-  return parsed
+    parsed_uri = urllib.parse.urlparse(uri)
+    if parsed_uri.scheme in (
+        'http',
+        'https',
+    ):
+        try:
+            fp = io.StringIO(requests_session.get(uri).text)
+        except requests.exceptions.ConnectionError:
+            fp = io.StringIO('')
+    else:
+        document = ls.workspace.get_text_document(uri)
+        try:
+            fp = io.StringIO(document.source)
+        except IOError:
+            if not allow_errors:
+                raise
+            fp = io.StringIO('')
+    parsed = await _parse(
+        fp,
+        uri,
+        allow_errors,
+    )
+    _parse_cache[uri] = copy.deepcopy(parsed)
+    return parsed
 
 
 async def _parse(
@@ -158,7 +158,8 @@ async def _parse(
         token_type = e.token.type if e.token.type else 'unknown'
         token_value = e.token.value if e.token.value else str(e.token)
 
-        error_message = f"Unexpected {token_type} '{token_value}' at line {e.line}, column {e.column}.\nExpected: {', '.join(e.expected)}"
+        error_message = (f"Unexpected {token_type} '{token_value}' at line {e.line}, column {e.column}.\n"
+                         f"Expected: {', '.join(e.expected)}")
 
         range = Range(
             start=Position(line=e.line - 1, character=e.column - 1),
@@ -193,38 +194,16 @@ async def _open(
     seen: List[str],
     allow_errors: bool,
 ) -> AST:
-  """Open a configuration file and return the result as a dictionary,
+    """Open a configuration file and return the result as a dictionary,
 
-  Recursively open other files based on buildout options found.
+    Recursively open other files based on buildout options found.
 
-  This is equivalent of zc.buildout.buildout._open
-  """
-  logger.debug("_open %r %r", base, uri)
+    This is equivalent of zc.buildout.buildout._open
+    """
+    logger.debug("_open %r %r", base, uri)
 
-  if not _isurl(uri):
-    assert base
-    uri = urllib.parse.urljoin(base, uri)
-  result = await parse(ls, uri, allow_errors=allow_errors)
-  return copy.deepcopy(result)
-
-@server.feature(TEXT_DOCUMENT_DID_OPEN)
-async def did_open(
-  ls: LanguageServer,
-  params: DidOpenTextDocumentParams,
-) -> None:
-  await parseAndSendDiagnostics(ls, params.text_document.uri)
-
-@server.feature(TEXT_DOCUMENT_DID_CHANGE)
-async def did_change(
-  ls: LanguageServer,
-  params: DidChangeTextDocumentParams,
-) -> None:
-  clearCache(params.text_document.uri)
-  await parseAndSendDiagnostics(ls, params.text_document.uri)
-
-async def parseAndSendDiagnostics(
-  ls: LanguageServer,
-  uri: str,
-) -> None:
-  parsed = await parse(ls,uri,allow_errors=True)
-  ls.publish_diagnostics(uri, parsed.diagnostics)
+    if not _isurl(uri):
+        assert base
+        uri = urllib.parse.urljoin(base, uri)
+    result = await parse(ls, uri, allow_errors=allow_errors)
+    return copy.deepcopy(result)
