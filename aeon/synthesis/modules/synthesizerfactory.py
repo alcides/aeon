07693c63f7778d18320df3286820416b6baab0e8
Alcides Fonseca <me@alcidesfonseca.com>
2025-07-03 10:07:32 +0100

    Added different flavours of geneticengine synthesis

--- aeon/synthesis/modules/synthesizerfactory.py
+++ aeon/synthesis/modules/synthesizerfactory.py
@@ -5,8 +5,16 @@ from aeon.synthesis.modules.synquid.synthesizer import SynquidSynthesizer
 
 def make_synthesizer(module: str) -> Synthesizer:
     match module:
-        case "GE":
-            return GESynthesizer()
+        case "random_search":
+            return GESynthesizer(method="random_search")
+        case "enumerative":
+            return GESynthesizer(method="enumerative")
+        case "gp":
+            return GESynthesizer(method="genetic_programming")
+        case "1p1":
+            return GESynthesizer(method="one_plus_one")
+        case "hc":
+            return GESynthesizer(method="hill_climbing")
         case "synquid":
             return SynquidSynthesizer()
         case _:
