950ca1cf6a140889673279a000534109f7cc64d2
Diogo Sousa <sousanamain@gmail.com>
2025-07-19 20:09:00 +0100

    fix so the tests pass

--- tests/pprint_test.py
+++ tests/pprint_test.py
@@ -1,19 +1,19 @@
 from abc import abstractmethod, ABC
 from dataclasses import dataclass
 
-from aeon.utils.pprint_helpers import Doc, text, concat, Assoc
+from aeon.utils.pprint_helpers import Doc, text, concat, Assoc, LayoutContext, Position
 
 
 def test_simple_pprint_1():
     expr = Mul(Add(Num(1), Num(2)), Div(Num(3), Num(4)))
     expected = "(1 + 2) * (3 / 4)"
-    assert expected == expr.to_doc().layout()
+    assert expected == expr.to_doc().best(0, 0).layout(context=LayoutContext(0, Position.NONE, 0))
 
 
 def test_simple_pprint_2():
     expr = Div(Add(Mul(Num(1), Num(2)), Num(2)), Div(Num(3), Num(4)))
     expected = "(1 * 2 + 2) / (3 / 4)"
-    assert expected == expr.to_doc().layout()
+    assert expected == expr.to_doc().best(0, 0).layout(context=LayoutContext(0, Position.NONE, 0))
 
 
 def test_complex_pretty_print():
@@ -21,7 +21,7 @@ def test_complex_pretty_print():
         Sub(Add(Num(1), Mul(Sub(Num(2), Num(3)), Num(4))), Div(Num(5), Num(6))), Add(Num(7), Mul(Num(8), Num(9)))
     )
     expected = "(1 + (2 - 3) * 4 - 5 / 6) / (7 + 8 * 9)"
-    assert expr.to_doc().layout() == expected
+    assert expected == expr.to_doc().best(0, 0).layout(context=LayoutContext(0, Position.NONE, 0))
 
 
 # helper classes
@@ -44,7 +44,7 @@ class Add(Expr):
     right: Expr
 
     def to_doc(self) -> Doc:
-        return concat([self.left.to_doc(), text(" + ", 1), self.right.to_doc()], precedence=1, assoc=Assoc.LEFT)
+        return concat([self.left.to_doc(), text(" + "), self.right.to_doc()], precedence=1, assoc=Assoc.LEFT)
 
 
 @dataclass(frozen=True)
@@ -53,7 +53,7 @@ class Sub(Expr):
     right: Expr
 
     def to_doc(self) -> Doc:
-        return concat([self.left.to_doc(), text(" - ", 1), self.right.to_doc()], precedence=1, assoc=Assoc.LEFT)
+        return concat([self.left.to_doc(), text(" - "), self.right.to_doc()], precedence=1, assoc=Assoc.LEFT)
 
 
 @dataclass(frozen=True)
@@ -62,7 +62,7 @@ class Mul(Expr):
     right: Expr
 
     def to_doc(self) -> Doc:
-        return concat([self.left.to_doc(), text(" * ", 2), self.right.to_doc()], precedence=2, assoc=Assoc.LEFT)
+        return concat([self.left.to_doc(), text(" * "), self.right.to_doc()], precedence=2, assoc=Assoc.LEFT)
 
 
 @dataclass(frozen=True)
@@ -71,4 +71,4 @@ class Div(Expr):
     right: Expr
 
     def to_doc(self) -> Doc:
-        return concat([self.left.to_doc(), text(" / ", 2), self.right.to_doc()], precedence=2, assoc=Assoc.LEFT)
+        return concat([self.left.to_doc(), text(" / "), self.right.to_doc()], precedence=2, assoc=Assoc.LEFT)
