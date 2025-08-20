f87f2786f51ad1a21e4135564e2a38f658767676
fourglobe <franciscomiguelolima@gmail.com>
2025-08-20 12:58:38 +0100

    Adding Helper Logger for more granular formating

---
+++ aeon/utils/indented_logger.py
@@ -0,0 +1,21 @@
+class IndentedLogger:
+    indents: list[str]
+    file = ""
+
+    def __init__(self, indents=[], file: str = "logs/default.log"):
+        self.indents = indents
+        self.file = file
+
+    def indent(self, i="  "):
+        self.indents = self.indents + [i]
+        return self
+
+    def dedent(self):
+        if self.indents:
+            self.indents = self.indents[:-1]
+        return self
+
+    def write(self, message: str):
+        with open(self.file, "a", encoding="utf-8") as f:
+            f.write("".join(self.indents) + message + "\n")
+        return self
