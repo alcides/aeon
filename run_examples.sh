b431da214cdc1a7ffb4818b1e601ebee743f099c
Alcides Fonseca <me@alcidesfonseca.com>
2025-10-28 22:49:33 +0000

    Made tests run faster

--- run_examples.sh
+++ run_examples.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}."
 
-PYTHON_BINARY=python3
-
 set -o errexit
 set -o nounset
 set -o pipefail
@@ -14,7 +12,7 @@ cd "$(dirname "$0")"
 
 function run_example {
     printf "Running %s ..." "${@}"
-    $PYTHON_BINARY -m aeon --no-main "$@" > /dev/null
+    uv run python -m aeon --no-main --budget 10 "$@" > /dev/null
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
         echo "(success)"
@@ -24,29 +22,6 @@ function run_example {
     fi
 }
 
-function run_example_core {
-    printf "Running %s ..." "${@}"
-    $PYTHON_BINARY -m aeon --core "$@" > /dev/null
-    RESULT=$?
-    if [ $RESULT -eq 0 ]; then
-        echo "(success)"
-    else
-        echo "(failed)"
-        exit 111
-    fi
-}
-
-# Should be somewhere else (maybe add to unit tests)
-# run_example examples/simple_choice_of_choice.py
-
-# Core disabled due to polymorphic inference
-# core_examples=examples/core
-# for entry in "$core_examples"/*.ae
-# do
-#   run_example_core "$entry"
-# done
-
-# TODO: add pbt PSB2
 for folder in ffi image imports list syntax synthesis "PSB2/solved";
 do
     for entry in examples/$folder/*.ae
