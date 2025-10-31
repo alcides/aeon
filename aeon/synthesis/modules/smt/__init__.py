a6c9a8166435ccb6a1c446a0a085db4e5b0c99a9
Alcides Fonseca <me@alcidesfonseca.com>
2025-10-31 18:09:38 +0000

    Fixed examples from demo

--- aeon/synthesis/modules/smt/__init__.py
+++ aeon/synthesis/modules/smt/__init__.py
@@ -1,8 +1,9 @@
-from typing import Callable
+from typing import Any, Callable, override
 from time import monotonic_ns
 
-from aeon.core.terms import Term
-from aeon.core.types import Type
+from aeon.core.liquid import LiquidLiteralBool, LiquidTerm
+from aeon.core.terms import Literal, Term
+from aeon.core.types import RefinedType, Type, TypeConstructor
 from aeon.decorators.api import Metadata
 from aeon.synthesis.api import Synthesizer
 from aeon.synthesis.uis.api import SynthesisUI
@@ -10,8 +11,12 @@ from aeon.typechecking.context import TypingContext
 from aeon.utils.name import Name
 from aeon.core.terms import Hole
 
+import z3
 
-def get_elapsed_time(start_time) -> float:
+from aeon.verification.smt import translate_liq
+
+
+def get_elapsed_time(start_time: float) -> float:
     """The elapsed time since the start in seconds."""
     return (monotonic_ns() - start_time) * 0.000000001
 
@@ -35,7 +40,29 @@ def is_better(a: list[float], b: list[float] | None, minimize: list[bool]) -> bo
     return wins - losses > 0
 
 
+def handle_base_type(type: Type) -> tuple[Any, Type, Callable[[Any], Any], Name, LiquidTerm]:
+    var: Any
+    vtype: Type
+    reverse: Callable[[Any], Any]
+    match type:
+        case TypeConstructor(Name("Bool", _)):
+            var = z3.Bool("var")
+            vtype = TypeConstructor(Name("Bool", -1))
+            reverse = bool
+        case TypeConstructor(Name("Int", _)):
+            var = z3.Int("var")
+            vtype = TypeConstructor(Name("Int", -1))
+            reverse = int
+        case RefinedType(name, btype, ref):
+            var, vtype, reverse, _, _ = handle_base_type(btype)
+            return var, vtype, reverse, name, ref
+        case _:
+            raise NotImplementedError()
+    return var, vtype, reverse, Name("_", -1), LiquidLiteralBool(True)
+
+
 class SMTSynthesizer(Synthesizer):
+    @override
     def synthesize(
         self,
         ctx: TypingContext,
@@ -49,6 +76,24 @@ class SMTSynthesizer(Synthesizer):
     ) -> Term:
         assert isinstance(ctx, TypingContext)
         assert isinstance(type, Type)
+        fail_term: Term = Hole(Name("sorry", -1))
 
-        core_term: Term = Hole(Name("sorry", -1))
-        return core_term
+        print("ty", type)
+
+        match handle_base_type(type):
+            case (var, vtype, reverse, name, cond):
+                solver = z3.Solver()
+                solver.add(translate_liq(cond, {str(name): var}))
+
+                match solver.check():
+                    case z3.z3.sat:
+                        m = solver.model()
+                        if var in m:
+                            return Literal(value=reverse(m[var]), type=vtype)
+                        else:
+                            return fail_term
+                    case _:
+                        return fail_term
+
+            case _:
+                return fail_term
