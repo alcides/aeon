ff0728c3fb4a86e7da35eccc048b6c59bc492480
sousanamain <sousanamain@gmail.com>
2026-03-13 04:37:19 +0000

    forgot to setup auto ruff

--- aeon/gpu/llvm_generation.py
+++ aeon/gpu/llvm_generation.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
+
 @dataclass
 class LLVMInstruction:
     pass
--- aeon/gpu/pipeline.py
+++ aeon/gpu/pipeline.py
@@ -1,10 +1,11 @@
 from __future__ import annotations
 
-from aeon.core.terms import Term, Rec, Abstraction, TypeAbstraction, Annotation
+from aeon.core.terms import Term
 from aeon.gpu.gpu_validator import validate_gpu_subset
 from aeon.gpu.gpu_converter import convert_to_gpu_ast, GType, GInt
 from aeon.frontend.anf_converter import ensure_anf
 from aeon.utils.name import Name
+
 
 def compile_to_gpu(aeon_term: Term, func_name: Name, arg_names: list[Name], arg_types: list[GType] = None) -> str:
     aeon_term = ensure_anf(aeon_term)
@@ -14,6 +15,6 @@ def compile_to_gpu(aeon_term: Term, func_name: Name, arg_names: list[Name], arg_
         arg_types = [GInt for _ in arg_names]
 
     type_env = {name: ty for name, ty in zip(arg_names, arg_types)}
-    gpu_ast = convert_to_gpu_ast(aeon_term, type_env=type_env)
+    _ = convert_to_gpu_ast(aeon_term, type_env=type_env)
 
     return "TODO"
