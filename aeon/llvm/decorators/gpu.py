49a8fcb0f58850481301b643dc858ae7fa0f3ba7
sousanamain <sousanamain@gmail.com>
2026-03-18 07:07:29 +0000

    add llvm/gpu decorator injection (I feel like this would be cleaner/less prone to errors if kwargs was a thing)

---
+++ aeon/llvm/decorators/gpu.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+from aeon.decorators.api import Metadata, metadata_update
+from aeon.sugar.program import Definition, STerm, SLiteral
+
+
+def gpu(
+    args: list[STerm],
+    fun: Definition,
+    metadata: Metadata,
+) -> tuple[Definition, list[Definition], Metadata]:
+    gpu_info = {
+        "gpu": True,
+        "gpu_device": "cuda",
+        "gpu_debug": False,
+        "gpu_cache": False,
+        "gpu_block_size": 1,
+        "gpu_thread_count": 1,
+    }
+
+    arg_keys = ["gpu_device", "gpu_debug", "gpu_cache", "gpu_block_size", "gpu_thread_count"]
+
+    for key, arg in zip(arg_keys, args):
+        if isinstance(arg, SLiteral):
+            gpu_info[key] = arg.value
+
+    metadata = metadata_update(metadata, fun, gpu_info)
+    return fun, [], metadata
---
+++ aeon/llvm/decorators/llvm.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from aeon.decorators.api import Metadata, metadata_update
+from aeon.sugar.program import Definition, STerm, SLiteral
+
+
+def llvm(
+    args: list[STerm],
+    fun: Definition,
+    metadata: Metadata,
+) -> tuple[Definition, list[Definition], Metadata]:
+    llvm_args = {"llvm": True, "llvm_debug": False, "llvm_cache": False}
+
+    arg_keys = ["llvm_debug", "llvm_cache"]
+
+    for key, arg in zip(arg_keys, args):
+        if isinstance(arg, SLiteral):
+            llvm_args[key] = arg.value
+
+    metadata = metadata_update(metadata, fun, llvm_args)
+    return fun, [], metadata
