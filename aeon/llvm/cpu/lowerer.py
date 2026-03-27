c4a5a81e3ae6f5844816fcd680b9419d19cd82c8
sousanamain <sousanamain@gmail.com>
2026-03-27 20:02:35 +0000

    done some changes to ensure lowering works even with anf partial application (needs further testing, but probably will be easier to create e2e tests)

--- aeon/llvm/cpu/lowerer.py
+++ aeon/llvm/cpu/lowerer.py
@@ -23,6 +23,7 @@ from aeon.llvm.llvm_ast import (
     LLVMTerm,
     LLVMLiteral,
     LLVMInt,
+    LLVMFloatType,
     LLVMVar,
     LLVMIf,
     LLVMCall,
@@ -127,33 +128,15 @@ class CPUFullApplicationValidationStep(ValidationStep):
         assert isinstance(ctx, CPUValidationContext)
         match t:
             case Application(fun, arg):
-                args = [arg]
-                current_fun = fun
-                while isinstance(current_fun, Application):
-                    args.append(current_fun.arg)
-                    current_fun = current_fun.fun
+                arguments = [arg]
+                base_function = fun
+                while isinstance(base_function, Application):
+                    arguments.append(base_function.arg)
+                    base_function = base_function.fun
 
-                target_type: LLVMType | None = None
-                if isinstance(current_fun, Var):
-                    name = current_fun.name.name
-                    if name in BINARY_OPS or name in UNARY_OPS:
-                        target_type = get_builtin_op_type(name)
-                    else:
-                        target_type = ctx.type_env.get(current_fun.name)
-                elif isinstance(current_fun, Annotation):
-                    target_type = from_type_to_llvm_type(current_fun.type)
-
-                if isinstance(target_type, LLVMFunctionType):
-                    if len(args) != len(target_type.arg_types):
-                        raise LLVMValidationError(
-                            f"Function application of {current_fun} is not fully applied. "
-                            f"Expected {len(target_type.arg_types)} arguments, got {len(args)}. "
-                            f"LLVM backend requires full application."
-                        )
-
-                for a in args:
+                for a in arguments:
                     self.validate(a, replace(ctx, is_top_level=False))
-                self.validate(current_fun, replace(ctx, is_top_level=False))
+                self.validate(base_function, replace(ctx, is_top_level=False))
 
             case Let(var_name, var_value, body):
                 llvm_var_type: LLVMType = LLVMInt
@@ -161,6 +144,8 @@ class CPUFullApplicationValidationStep(ValidationStep):
                     llvm_var_type = from_type_to_llvm_type(var_value.type)
                 elif isinstance(var_value, Rec):
                     llvm_var_type = from_type_to_llvm_type(var_value.var_type)
+                elif isinstance(var_value, Var) and var_value.name.name in BINARY_OPS:
+                    llvm_var_type = get_builtin_op_type(var_value.name.name)
 
                 new_ctx = replace(ctx, type_env=ctx.type_env | {var_name: llvm_var_type}, is_top_level=False)
                 self.validate(var_value, replace(ctx, is_top_level=False))
@@ -193,6 +178,33 @@ class CPULLVMLowerer(LLVMLowerer):
     def get_validation_steps(self) -> List[ValidationStep]:
         return [CPUTypeValidationStep(), CPUFunctionCallValidationStep(), CPUFullApplicationValidationStep()]
 
+    def _uncurry_application(self, application: Application) -> tuple[Term, List[Term]]:
+        arguments = [application.arg]
+        base_function = application.fun
+        while isinstance(base_function, Application):
+            arguments.append(base_function.arg)
+            base_function = base_function.fun
+        arguments.reverse()
+        return base_function, arguments
+
+    def _get_operator_type(self, op_name: str, expected_type: LLVMType | None) -> LLVMFunctionType:
+        is_float = False
+        if expected_type:
+            if isinstance(expected_type, LLVMFunctionType):
+                is_float = any(isinstance(ty, LLVMFloatType) for ty in expected_type.arg_types)
+            elif isinstance(expected_type, LLVMFloatType):
+                is_float = True
+        return get_builtin_op_type(op_name, is_float)
+
+    def _is_inlinable_anf(self, var_name: Name, llvm_value: LLVMTerm) -> bool:
+        if not var_name.name.startswith("anf"):
+            return False
+        is_partial_call = isinstance(llvm_value, LLVMCall) and isinstance(llvm_value.type, LLVMFunctionType)
+        is_operator = isinstance(llvm_value, LLVMVar) and (
+            llvm_value.name.name in BINARY_OPS or llvm_value.name.name in UNARY_OPS
+        )
+        return is_partial_call or is_operator
+
     def lower(
         self,
         t: Term,
@@ -212,34 +224,49 @@ class CPULLVMLowerer(LLVMLowerer):
 
             case Var(name):
                 if name.name in BINARY_OPS or name.name in UNARY_OPS:
-                    llvm_type = get_builtin_op_type(name.name)
+                    llvm_type = self._get_operator_type(name.name, expected_type)
+                    return LLVMVar(llvm_type, name)
+                elif name in env:
+                    return env[name]
                 else:
                     llvm_type = type_env.get(name) or LLVMInt
-                return LLVMVar(llvm_type, name)
+                    return LLVMVar(llvm_type, name)
 
             case Annotation(expr, type) | TypeApplication(expr, type):
                 llvm_type = from_type_to_llvm_type(type)
                 return self.lower(expr, llvm_type, type_env, env)
 
-            case Application(fun, arg):
-                args = [arg]  # uncurry
-                current_fun = fun
-                while isinstance(current_fun, Application):
-                    args.append(current_fun.arg)
-                    current_fun = current_fun.fun
-                args.reverse()
+            case Application(_, _):
+                base_function, application_args = self._uncurry_application(t)
+                lowered_base_function = self.lower(base_function, None, type_env, env)
 
-                llvm_fun = self.lower(current_fun, None, type_env, env)
+                is_partial_application = isinstance(lowered_base_function, LLVMCall) and isinstance(
+                    lowered_base_function.type, LLVMFunctionType
+                )
+                if is_partial_application:
+                    function_target = lowered_base_function.target
+                    previously_applied_args = lowered_base_function.args
+                    full_function_type = function_target.type
+                else:
+                    function_target = lowered_base_function
+                    previously_applied_args = []
+                    full_function_type = lowered_base_function.type
 
-                assert isinstance(llvm_fun.type, LLVMFunctionType)
-                assert len(args) == len(llvm_fun.type.arg_types)
+                all_lowered_args = previously_applied_args.copy()
+                for arg in application_args:
+                    arg_index = len(all_lowered_args)
+                    expected_arg_type = full_function_type.arg_types[arg_index]
+                    all_lowered_args.append(self.lower(arg, expected_arg_type, type_env, env))
 
-                llvm_args = []
-                for i, a in enumerate(args):
-                    expected_arg_ty = llvm_fun.type.arg_types[i]
-                    llvm_args.append(self.lower(a, expected_arg_ty, type_env, env))
-
-                return LLVMCall(type=llvm_fun.type.return_type, target=llvm_fun, args=llvm_args)
+                is_still_partial = len(all_lowered_args) < len(full_function_type.arg_types)
+                if is_still_partial:
+                    unapplied_arg_types = full_function_type.arg_types[len(all_lowered_args) :]
+                    partial_return_type = LLVMFunctionType(
+                        arg_types=unapplied_arg_types, return_type=full_function_type.return_type
+                    )
+                    return LLVMCall(type=partial_return_type, target=function_target, args=all_lowered_args)
+                else:
+                    return LLVMCall(type=full_function_type.return_type, target=function_target, args=all_lowered_args)
 
             case Abstraction(var_name, body):
                 names = [var_name]
@@ -267,7 +294,14 @@ class CPULLVMLowerer(LLVMLowerer):
                 new_type_env = type_env.copy()
                 new_type_env[var_name] = llvm_value.type
 
-                llvm_body = self.lower(body, expected_type, new_type_env, env)
+                new_env = env.copy()
+
+                if self._is_inlinable_anf(var_name, llvm_value):
+                    new_env[var_name] = llvm_value
+                    return self.lower(body, expected_type, new_type_env, new_env)
+
+                new_env[var_name] = LLVMVar(llvm_value.type, var_name)
+                llvm_body = self.lower(body, expected_type, new_type_env, new_env)
                 return LLVMLet(type=llvm_body.type, var_name=var_name, var_value=llvm_value, body=llvm_body)
 
             case Rec(var_name, var_type, var_value, body):
@@ -276,8 +310,11 @@ class CPULLVMLowerer(LLVMLowerer):
                 new_type_env = type_env.copy()
                 new_type_env[var_name] = llvm_func_type
 
-                llvm_func_value = self.lower(var_value, llvm_func_type, new_type_env, env)
-                llvm_body = self.lower(body, expected_type, new_type_env, env)
+                new_env = env.copy()
+                new_env[var_name] = LLVMVar(llvm_func_type, var_name)
+
+                llvm_func_value = self.lower(var_value, llvm_func_type, new_type_env, new_env)
+                llvm_body = self.lower(body, expected_type, new_type_env, new_env)
 
                 return LLVMLet(type=llvm_body.type, var_name=var_name, var_value=llvm_func_value, body=llvm_body)
 
