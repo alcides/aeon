0474b2e41fb29687c4ec0d7e5145055d8bb0a540
sousanamain <sousanamain@gmail.com>
2026-05-06 11:38:42 +0100

    feat(core): add `Vector` type support and remove GPU/Tensor-related code

--- aeon/sugar/stypes.py
+++ aeon/sugar/stypes.py
@@ -87,7 +87,7 @@ class STypeConstructor(SType):
         return hash(self.name) + sum(hash(c) for c in self.args)
 
 
-builtin_types = ["Top", "Bool", "Int", "Float", "String", "Unit", "Set", "Tensor", "GpuConfig"]
+builtin_types = ["Top", "Bool", "Int", "Float", "String", "Unit", "Vector", "Set"]
 
 
 def get_type_vars(ty: SType) -> set[STypeVar]:
